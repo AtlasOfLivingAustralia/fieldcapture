b55417b2ebcebabf9094abf13833df0135dbbe43
markew <mark.woolston@axon.com.au>
2013-04-04 03:39:01 +0000

    add knockout for data binding
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@15 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -13,4 +13,9 @@ modules = {
         resource url: 'js/wms.js'
         resource url: 'js/keydragzoom.js'
     }
+
+    knockout {
+        resource url:'js/knockout-2.2.1.debug.js'
+        resource url:'js/knockout.mapping-latest.js'
+    }
 }
