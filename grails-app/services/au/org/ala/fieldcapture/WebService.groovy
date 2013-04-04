80622d623b2863b0c32bc2cec7aa50fa9b418b50
markew <mark.woolston@axon.com.au>
2013-04-04 03:36:27 +0000

    std http utilities
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@12 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ grails-app/services/au/org/ala/fieldcapture/WebService.groovy
@@ -0,0 +1,87 @@
+package au.org.ala.fieldcapture
+
+import grails.converters.JSON
+import org.codehaus.groovy.grails.web.converters.exceptions.ConverterException
+
+class WebService {
+
+    def get(String url) {
+        def conn = new URL(url).openConnection()
+        try {
+            conn.setConnectTimeout(10000)
+            conn.setReadTimeout(50000)
+            return conn.content.text
+        } catch (SocketTimeoutException e) {
+            def error = [error: "Timed out calling web service. URL= ${url}."]
+            log.error error
+            return error
+        } catch (Exception e) {
+            def error = [error: "Failed calling web service. ${e.getClass()} ${e.getMessage()} URL= ${url}.",
+                         detail: conn.errorStream.text]
+            log.error error
+            return error
+        }
+    }
+
+    def getJson(String url) {
+        def conn = new URL(url).openConnection()
+        try {
+            conn.setConnectTimeout(10000)
+            conn.setReadTimeout(50000)
+            def json = conn.content.text
+            return JSON.parse(json)
+        } catch (ConverterException e) {
+            def error = ['error': "Failed to parse json. ${e.getClass()} ${e.getMessage()} URL= ${url}."]
+            log.error error
+            return error
+        } catch (SocketTimeoutException e) {
+            def error = [error: "Timed out getting json. URL= ${url}."]
+            println error
+            return error
+        } catch (Exception e) {
+            def error = [error: "Failed to get json from web service. ${e.getClass()} ${e.getMessage()} URL= ${url}.",
+                         detail: conn.errorStream.text]
+            log.error error
+            return error
+        }
+    }
+
+    def doPost(String url, String postBody) {
+        def resp = ""
+        def conn = new URL(url).openConnection()
+        try {
+            conn.setDoOutput(true)
+            conn.setRequestProperty("Content-Type", "application/json");
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream())
+            wr.write(postBody)
+            wr.flush()
+            resp = conn.inputStream.text
+            wr.close()
+            return [resp: JSON.parse(resp)]
+        } catch (SocketTimeoutException e) {
+            def error = [error: "Timed out calling web service. URL= ${url}."]
+            log.error error
+            return error
+        } catch (Exception e) {
+            def error = [error: "Failed calling web service. ${e.getMessage()} URL= ${url}.",
+                         detail: conn.errorStream.text]
+            log.error error
+            return error
+        }
+    }
+
+    def doDelete(String url) {
+        def conn = new URL(url).openConnection()
+        try {
+            conn.setRequestMethod("DELETE")
+            return conn.getResponseCode()
+        } catch(Exception e){
+            println e.message
+            return 500
+        } finally {
+            if (conn != null){
+                conn.disconnect()
+            }
+        }
+    }
+}
