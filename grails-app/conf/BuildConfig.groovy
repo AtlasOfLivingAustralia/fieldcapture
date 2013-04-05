dbba4c80a377a8d688a08b3715d536a1271bed90
markew <mark.woolston@axon.com.au>
2013-04-05 00:03:37 +0000

    move csv plugin ref to BuildConfig
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@30 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -39,6 +39,8 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.8.0"
         runtime ":resources:1.1.6"
         runtime ":ala-web-theme:0.1.3"
+        runtime ":csv:0.3.1"
+        //runtime ":lesscss-resources:1.3.1"
 
         build ":tomcat:$grailsVersion"
 
