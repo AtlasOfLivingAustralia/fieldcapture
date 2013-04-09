7c7294a0a9433fbdf13619a9ce2d2d342795eebf
markew <mark.woolston@axon.com.au>
2013-04-09 01:52:12 +0000

    add general solution for showing features on a map
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@38 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -14,6 +14,10 @@ modules = {
         resource url: 'js/keydragzoom.js'
     }
 
+    mapWithFeatures {
+        resource url: 'js/mapWithFeatures.js'
+    }
+
     knockout {
         resource url:'js/knockout-2.2.1.debug.js'
         resource url:'js/knockout.mapping-latest.js'
