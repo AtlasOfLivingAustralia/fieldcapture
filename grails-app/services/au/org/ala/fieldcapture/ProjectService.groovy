d9d1992f3e985adb992dfa91d50e89d3b08a7a54
markew <mark.woolston@axon.com.au>
2013-04-14 04:03:10 +0000

    convert to use the generic mapWithFeatures
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@63 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/services/au/org/ala/fieldcapture/ProjectService.groovy
+++ grails-app/services/au/org/ala/fieldcapture/ProjectService.groovy
@@ -1,8 +1,12 @@
 package au.org.ala.fieldcapture
 
+import grails.converters.JSON
+import org.codehaus.groovy.grails.web.mapping.LinkGenerator
+
 class ProjectService {
 
     def webService, grailsApplication
+    LinkGenerator grailsLinkGenerator
 
     def siteService  // temporary coupling for test data
     def projects
@@ -30,6 +34,36 @@ class ProjectService {
 
     def delete(id) {
         webService.doDelete(grailsApplication.config.ecodata.baseUrl + 'project/' + id)
+    }
+
+    // todo: move this defn to a common place
+    static locationTypes = [locationTypePoint: 'point', locationTypePolygon: 'polygon', locationTypePid: 'pid']
+    /**
+     * Returns json that describes in a generic fashion the features to be placed on a map that
+     * will represent the site's locations.
+     *
+     * @param project
+     */
+    def getMapFeatures(project) {
+        def featuresMap = [zoomToBounds: true, zoomLimit: 12, highlightOnHover: true, features: []]
+        project.sites.each { site ->
+            site.location.each { loc ->
+                def location = [type: locationTypes[loc.type], name: site.name + ' - ' + loc.name, id: site.name]
+                switch (location.type) {
+                    case 'point':
+                        location.latitude = loc.data.decimalLatitude
+                        location.longitude = loc.data.decimalLongitude
+                        break
+                    case 'pid':
+                        location.polygonUrl = grailsLinkGenerator.link(
+                                controller: 'proxy', action: 'geojsonFromPid',
+                                params: [pid: loc.data.pid]
+                        )
+                }
+                featuresMap.features << location
+            }
+        }
+        return featuresMap as JSON
     }
 
     def enrichTestData() {
