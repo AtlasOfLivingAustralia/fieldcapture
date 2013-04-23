df0ec0639f81b2ff926f6e77b2c67ea8dd3ca5f5
markew <mark.woolston@axon.com.au>
2013-04-23 04:33:15 +0000

    inject output scores into activity model
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@84 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/services/au/org/ala/fieldcapture/ActivityService.groovy
+++ grails-app/services/au/org/ala/fieldcapture/ActivityService.groovy
@@ -1,11 +1,14 @@
 package au.org.ala.fieldcapture
 
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 class ActivityService {
 
-    def webService, grailsApplication
+    def webService, grailsApplication, outputService
 
     def list() {
         def resp = webService.getJson(grailsApplication.config.ecodata.baseUrl + 'activity/')
@@ -14,6 +17,24 @@ class ActivityService {
 
     def get(id) {
         def record = webService.getJson(grailsApplication.config.ecodata.baseUrl + 'activity/' + id)
+        // extract primary outputs
+        /*
+         NOTE we have to be careful to inject JSONObjects not LinkedHashMaps so they will toString correctly
+         when we use the strings to initialise Javascript objects in the GSP.
+         */
+        record.outputs.each {
+            def o = outputService.get(it.outputId)
+            if (o?.data) {
+                it.scores = new JSONArray()
+                o.data.each { k, v ->
+                    // todo: using the prefix 'total' as a marker
+                    // todo: will need to use the data meta-model
+                    if (k.size() > 4 && k[0..4] == 'total') {
+                        it.scores << new JSONObject([name: k, score: v])
+                    }
+                }
+            }
+        }
         record
     }
 
@@ -25,7 +46,7 @@ class ActivityService {
         webService.doDelete(grailsApplication.config.ecodata.baseUrl + 'activity/' + id)
     }
 
-    def convertToSimpleDate(value) {
+    /*def convertToSimpleDate(value) {
         def pattern = ~/\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\dZ/
         if (value instanceof String && pattern.matcher(value).matches()) {
             return "${value[8..9]}-${value[5..6]}-${value[0..3]}"
@@ -57,5 +78,5 @@ class ActivityService {
             entry.value = convertFromSimpleDate(entry.value)
         }
         map
-    }
+    }*/
 }
