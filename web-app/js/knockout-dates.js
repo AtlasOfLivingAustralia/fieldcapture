b4b995bdeeaf1a45e9584193780c4aeda7961909
markew <mark.woolston@axon.com.au>
2013-04-23 04:35:31 +0000

    add protectedObservable to support commit/rollback editing
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@86 63d11371-6d71-872f-44d1-802e8bf8a75e

--- web-app/js/knockout-dates.js
+++ web-app/js/knockout-dates.js
@@ -229,3 +229,33 @@ function pad(number, length){
     }
     return str
 }
+
+//wrapper for an observable that protects value until committed
+ko.protectedObservable = function(initialValue) {
+    //private variables
+    var _temp = initialValue;
+    var _actual = ko.observable(initialValue);
+
+    var result = ko.dependentObservable({
+        read: _actual,
+        write: function(newValue) {
+            _temp = newValue;
+        }
+    });
+
+    //commit the temporary value to our observable, if it is different
+    result.commit = function() {
+        if (_temp !== _actual()) {
+            _actual(_temp);
+        }
+    };
+
+    //notify subscribers to update their value with the original
+    result.reset = function() {
+        _actual.valueHasMutated();
+        _temp = _actual();
+    };
+
+    return result;
+};
+
