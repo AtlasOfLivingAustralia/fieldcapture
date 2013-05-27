d934f701f988905d52b73e53ace6634c9a4dbeae
markew <mark.woolston@axon.com.au>
2013-05-27 05:05:05 +0000

    auto-generated test classes
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@166 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ test/unit/au/org/ala/fieldcapture/ModelJSTagLibTests.groovy
@@ -0,0 +1,17 @@
+package au.org.ala.fieldcapture
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(ModelJSTagLib)
+class ModelJSTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
