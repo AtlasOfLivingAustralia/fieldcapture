4431eca7ed455a287b2b1e8646515fb7e9a448f8
markew <mark.woolston@axon.com.au>
2013-06-09 06:02:36 +0000

    refactor bootstrap resources (independent of ala plugin)
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@185 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,8 @@
 modules = {
     application {
+        dependsOn 'jquery'
+        resource url: "${grailsApplication.config.ala.baseURL?:'http://www.ala.org.au'}/wp-content/themes/ala2011/images/favicon.ico", attrs:[type:'ico'], disposition: 'head'
+        resource url: [dir:'js', file:'html5.js', plugin: "ala-web-theme"], wrapper: { s -> "<!--[if lt IE 9]>$s<![endif]-->" }, disposition: 'head'
         resource url:'js/application.js'
         resource url: 'css/capture.css'
         resource url: 'js/jquery.shorten.js'
@@ -36,4 +39,18 @@ modules = {
         resource url: 'js/bootstrap-datepicker.js'
         resource url: '/css/datepicker.css'
     }
+
+    app_bootstrap {
+        dependsOn 'application'
+        resource url: '/bootstrap/js/bootstrap.min.js'
+        resource url: '/bootstrap/css/bootstrap.min.css'
+        resource url: '/bootstrap/img/glyphicons-halflings-white.png'
+        resource url: '/bootstrap/img/glyphicons-halflings.png'
+    }
+
+    app_bootstrap_responsive {
+        dependsOn 'bootstrap'
+        resource url: '/bootstrap/css/bootstrap-responsive.min.css'
+    }
+
 }
