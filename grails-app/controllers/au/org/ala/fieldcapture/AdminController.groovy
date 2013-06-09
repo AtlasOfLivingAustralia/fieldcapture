c8b4dd45e836cbe52ff4c58ce3889a3ecc0cf718
markew <mark.woolston@axon.com.au>
2013-06-09 06:05:38 +0000

    add admin pages
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@187 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ grails-app/controllers/au/org/ala/fieldcapture/AdminController.groovy
@@ -0,0 +1,97 @@
+package au.org.ala.fieldcapture
+
+import grails.util.Environment
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
+
+class AdminController {
+
+    def cacheService
+
+    def index() {}
+    def tools() {}
+    def users() {}
+    def settings() {
+        def settings = [
+            [key:'app.external.model.dir', value: grailsApplication.config.app.external.model.dir,
+            comment: 'location of the application meta-models such as the list of activities and ' +
+                    'the output data models']
+        ]
+        def config = grailsApplication.config.flatten()
+        ['ecodata.baseUrl','grails.serverURL','grails.config.locations','biocache.baseURL',
+            'spatial.baseURL','ala.baseURL','collectory.baseURL','headerAndFooter.baseURL',
+            'spatial.layers.service.url','sld.polgon.default.url',
+            'sld.polgon.highlight.url','spatialLayerServices.baseUrl'
+        ].each {
+            settings << [key: it, value: config[it], comment: '']
+        }
+        [settings: settings]
+    }
+
+    def reloadConfig = {
+        // reload system config
+        def resolver = new PathMatchingResourcePatternResolver()
+        List resources = resolver.getResource(grailsApplication.config.grails.config.locations)
+        if (!resources) {
+            def warning = "No external config to reload. grailsApplication.config.grails.config.locations is empty."
+            println warning
+            flash.message = warning
+            render warning
+        } else {
+            def resource = resources[0]
+            def stream = null
+
+            try {
+                stream = resource.getInputStream()
+                ConfigSlurper configSlurper = new ConfigSlurper(Environment.current.name)
+                if(resource.filename.endsWith('.groovy')) {
+                    def newConfig = configSlurper.parse(stream.text as String)
+                    grailsApplication.getConfig().merge(newConfig)
+                }
+                else if(resource.filename.endsWith('.properties')) {
+                    def props = new Properties()
+                    props.load(stream)
+                    def newConfig = configSlurper.parse(props)
+                    grailsApplication.getConfig().merge(newConfig)
+                }
+                flash.message = "Configuration reloaded."
+                String res = "<ul>"
+                grailsApplication.config.each { key, value ->
+                    if (value instanceof Map) {
+                        res += "<p>" + key + "</p>"
+                        res += "<ul>"
+                        value.each { k1, v1 ->
+                            res += "<li>" + k1 + " = " + v1 + "</li>"
+                        }
+                        res += "</ul>"
+                    }
+                    else {
+                        res += "<li>${key} = ${value}</li>"
+                    }
+                }
+                render res + "</ul>"
+            }
+            catch (FileNotFoundException fnf) {
+                def error = "No external config to reload configuration. Looking for ${grailsApplication.config.grails.config.locations[0]}"
+                log.error error
+                flash.message = error
+                render error
+            }
+            catch (Exception gre) {
+                def error = "Unable to reload configuration. Please correct problem and try again: " + gre.getMessage()
+                log.error error
+                flash.message = error
+                render error
+            }
+            finally {
+                stream?.close()
+            }
+        }
+    }
+
+    def clearMetadataCache() {
+        // clear any cached external config
+        cacheService.clear()
+        flash.message = "Metadata cache cleared."
+        render 'done'
+    }
+}
---
+++ grails-app/views/admin/tools.gsp
@@ -0,0 +1,65 @@
+<%@ page import="org.apache.commons.lang.StringEscapeUtils" %>
+<!doctype html>
+<html>
+    <head>
+        <meta name="layout" content="adminLayout"/>
+        <title>Tools - Admin - Data capture - Atlas of Living Australia</title>
+    </head>
+
+    <body>
+        <script type="text/javascript">
+
+            $(document).ready(function() {
+
+                $("#btnReloadConfig").click(function(e) {
+                    e.preventDefault();
+                    $.ajax("${createLink(controller: 'admin', action:'reloadConfig')}").done(function(result) {
+                        document.location.reload();
+                    });
+                });
+
+                $("#btnClearMetadataCache").click(function(e) {
+                    e.preventDefault();
+                    $.ajax("${createLink(controller: 'admin', action:'clearMetadataCache')}").done(function(result) {
+                        document.location.reload();
+                    }).fail(function (result) {
+                        alert(result);
+                    });
+                });
+
+            });
+
+        </script>
+        <content tag="pageTitle">Tools</content>
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Tool</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <button id="btnReloadConfig" class="btn btn-small btn-info">Reload&nbsp;External&nbsp;Config</button>
+                    </td>
+                    <td>
+                        Reads any defined config files and merges new config with old. Usually used after a change is
+                        made to external config files. Note that this cannot remove a config item as the result is a
+                        union of the old and new config.
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button id="btnClearMetadataCache" class="btn btn-small btn-info">Clear&nbsp;Metadata&nbsp;Cache</button>
+                    </td>
+                    <td>
+                        Removes all cached values for metadata requests and causes the metadata to be requested
+                        from the source at the next attempt to use the metadata.
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+    </body>
+</html>
