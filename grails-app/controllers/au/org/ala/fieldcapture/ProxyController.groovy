d46d926dceaeab512d24efe8659ecba96aefa42f
djtfmartin <djtfmartin@gmail.com>
2013-07-18 07:23:34 +0000

    url changes
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@422 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/controllers/au/org/ala/fieldcapture/ProxyController.groovy
+++ grails-app/controllers/au/org/ala/fieldcapture/ProxyController.groovy
@@ -7,9 +7,9 @@ class ProxyController {
     def webService
 
     def geojsonFromPid(String pid) {
-        log.debug "requesting pid ${pid}"
-        def resp = webService.get(grailsApplication.config.spatialLayerServices.baseUrl +
-                "shape/geojson/${pid}")
+        def shpUrl = grailsApplication.config.spatialLayerServices.baseUrl + "shape/geojson/${pid}"
+        log.debug "requesting pid ${pid} URL: ${shpUrl}"
+        def resp = webService.get(shpUrl)
         //log.debug resp
         render resp as String
     }
