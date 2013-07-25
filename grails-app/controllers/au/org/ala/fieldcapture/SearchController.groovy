ce8a51faf8a8553487cde0b60b56f04a42c61f88
chrisala <chris.godwin.ala@gmail.com>
2013-07-25 07:22:04 +0000

    Added the ability to autocomplete against a list defined by the ala lists app.  (including temporary code that could be implemented in the lists webservices).
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@441 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/controllers/au/org/ala/fieldcapture/SearchController.groovy
+++ grails-app/controllers/au/org/ala/fieldcapture/SearchController.groovy
@@ -1,5 +1,8 @@
 package au.org.ala.fieldcapture
 
+import grails.converters.JSON
+import groovy.json.JsonSlurper
+
 class SearchController {
     def webService
     def commonService
@@ -24,4 +27,45 @@ class SearchController {
         log.debug "response = " + resp
         [results: resp]
     }
+
+    /**
+     * Handles queries to support autocomplete for species fields.
+     * @param q the typed query.
+     * @param limit the maximum number of results to return
+     * @return
+     */
+    def species(String q, Integer limit) {
+
+        if (!limit) {
+            limit = 10;
+        }
+
+        def listId = params.druid
+        if (listId) {
+            render filterSpeciesList(q, listId)
+        }
+        else {
+            render webService.get("http://bie.ala.org.au/search/auto.jsonp?q=${q}&limit=${limit}")
+        }
+
+    }
+
+    /**
+     * Searches the "name" returned by the Species List service for the supplied search term and reformats the
+     * results to match those returned by the bie.
+     * @param query the term to search for.
+     * @param listId the id of the list to search.
+     * @return a JSON formatted String of the form {"autoCompleteList":[{...results...}]}
+     */
+    private String filterSpeciesList(String query, String listId) {
+        def listContents = new JsonSlurper().parseText(webService.get("http://lists.ala.org.au/ws/speciesListItems/${listId}"))
+
+        def filtered = listContents.findResults({it.name?.toLowerCase().contains(query.toLowerCase()) ? [id: it.id, name: it.name, matchedNames:[it.name], guid:it.lsid]: null})
+
+        def results = [:];
+        results.autoCompleteList = filtered
+
+        return results as JSON
+    }
+
 }
