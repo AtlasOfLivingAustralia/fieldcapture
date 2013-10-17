ce01a5153f60664a27b129667264918cbf04b95f
djtfmartin <djtfmartin@gmail.com>
2013-10-17 06:18:59 +0000

    fix for changing user permissions
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@796 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,15 +7,18 @@ class UrlMappings {
 			}
 		}
 
-        "/$controller/$id?"(parseRequest:true) {
-            action = [GET: "get", POST: "upload", PUT: "upload", DELETE: "delete"]
-        }
+    "/$controller/$id?"(parseRequest:true) {
+        action = [GET: "get", POST: "upload", PUT: "upload", DELETE: "delete"]
+    }
 
 		"/"(controller: 'home', action: 'index')
 		"/about"(controller: 'home', action: 'about')
-        "/myProfile"(controller: 'home', action: 'myProfile')
-        "/user/index"(controller: "user", action: "index")
-        "/user/${id}"(controller: "user", action: "show")
+    "/myProfile"(controller: 'home', action: 'myProfile')
+    "/user/index"(controller: "user", action: "index")
+    "/user/checkEmailExists"(controller: "user", action: "checkEmailExists")
+    "/user/removeUserWithRole"(controller: "user", action: "removeUserWithRole")
+    "/user/addUserAsRoleToProject"(controller: "user", action: "addUserAsRoleToProject")
+    "/admin/user/${id}"(controller: "user", action: "show")
 		"500"(view:'/error')
 	}
 }
