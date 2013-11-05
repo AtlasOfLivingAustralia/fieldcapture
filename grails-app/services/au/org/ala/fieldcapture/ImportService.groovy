ec7e08e0da855418b3faee4db3decd1c3e823ee0
chrisala <chris.godwin.ala@gmail.com>
2013-11-05 00:12:00 +0000

    Changes to the import service to handle the new GMS dataset.
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@869 63d11371-6d71-872f-44d1-802e8bf8a75e

--- grails-app/services/au/org/ala/fieldcapture/ImportService.groovy
+++ grails-app/services/au/org/ala/fieldcapture/ImportService.groovy
@@ -3,15 +3,22 @@ package au.org.ala.fieldcapture
 import au.com.bytecode.opencsv.CSVReader
 import org.apache.commons.lang.StringUtils
 
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+
 /**
  * Handles data import into ecodata.
  */
 class ImportService {
 
     static int INSTITUTION_DIFFERENCE_THRESHOLD = 4
-    public static final List CSV_HEADERS = ['Grant ID', 'Grant Name', 'Grant Description', 'Grant Status', 'Grantee Full Name', 'Application Location Desc', 'Original Approved Amount', 'Round Name']
+    public static final List CSV_HEADERS = ['Grant ID', 'Grant External ID', 'Grant Name', 'Grant Description', 'Grantee Organisation Legal Name', 'Location Description', 'Grant Original Approved Amount', 'Round Name', 'Start', 'Finish']
 
     private static final String PROJECTS_CACHE_KEY = 'ImportService-AllProjects'
+
+    static outputDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ssZ")
+    static inputDateFormat = new SimpleDateFormat("dd/MM/yyyy")
+
 
     /** The current format location data is supplied in */
     def locationRegExp = /lat. = ([\-0-9\.]*)\nlong. = ([\-0-9\.]*)\nLocation Description = (.*)lat\. =.*/
@@ -32,7 +39,7 @@ class ImportService {
 
         def results = [success:false, validationErrors:[]]
 
-        def projectsCreated = 0, projectsUpdated = 0, sitesCreated = 0, sitesUpdated = 0
+        def projectsCreated = 0, projectsUpdated = 0, sitesCreated = 0, sitesUpdated = 0, ignored = 0
 
         // Clear the cache before importing to ensure we get fresh data. (in particular because I keep dropping the database)
         cacheService.clear(PROJECTS_CACHE_KEY)
@@ -41,10 +48,24 @@ class ImportService {
         // Used to detect duplicate grantIds (as it is used as the key)
         def grantIds = []
         while (csvLine) {
+            // This is to handle blank lines and subheadings which have appeared in the latest version
+            if (csvLine.size() < CSV_HEADERS.size()) {
+                log.warn("CSV line encountered with too few columns: ${csvLine}")
+                csvLine = csvReader.readNext()
+                continue
+            }
+            def grantId = csvLine[headerIndexes['Grant ID']]
+            // This is to handle duplicate header columns and other formatting lines which appear through the latest version
+            if (grantId.size() != 12) {
+                log.debug("Invalid line encountered: ${csvLine}")
+                csvLine = csvReader.readNext()
+                continue
+            }
+
             def project = validateProjectDetails(findHeaderColumns(headerIndexes, csvLine), grantIds)
             grantIds << project.grantId
-            if (project.errors.size() > 0) {
-                writeErrors(results.validationErrors, project.errors, csvLine)
+            if (project.errors.size() > 0 || project.warnings.size() > 0) {
+                writeErrors(results.validationErrors, project.errors, project.warnings, csvLine)
             }
             if (project.errors.size() == 0 || importWithErrors) {
                 project.remove('errors')
@@ -63,6 +84,9 @@ class ImportService {
                 }
 
             }
+            else {
+                ignored++
+            }
 
             csvLine = csvReader.readNext()
         }
@@ -70,10 +94,11 @@ class ImportService {
         results.projectsUpdated = projectsUpdated
         results.sitesCreated = sitesCreated
         results.sitesUpdated = sitesUpdated
+        results.ignored = ignored
 
         // Clear the cache again to free up memory and also in case old project data has been cached.
-        cacheService.clear()
-        println results
+        cacheService.clear(PROJECTS_CACHE_KEY)
+        println "Created: ${results.projectsCreated}, Updated: ${results.projectsUpdated}, Errors: ${ignored}"
         return results
 
     }
@@ -82,27 +107,56 @@ class ImportService {
 
         def project = [:]
         def errors = []
+        def warnings = []
         project.errors = errors
+        project.warnings = warnings
 
         CSV_HEADERS.each {
             if (!projectDetails[it]) {
-                errors.add("No value for '${it}'")
+                if (it == 'Grant ID' || it == 'Grant Name') {
+                    errors.add("No value for '${it}'")
+                }
+                else {
+                    warnings.add("No value for '${it}'")
+                }
             }
         }
 
         project.name = projectDetails['Grant Name']
         project.description = projectDetails['Grant Description']
         project.grantId = projectDetails['Grant ID']
-        project.funding = projectDetails['Original Approved Amount']
-        project.organisationName = projectDetails['Grantee Full Name']
+        project.externalProjectId = projectDetails['Grant External ID']
 
+
+        def funding = projectDetails['Grant Original Approved Amount']
+        try {
+            project.funding = new DecimalFormat('$##,###.##').parse(funding)
+        }
+        catch (Exception e) {
+            errors.add("Unable to parse funding: "+funding)
+        }
+        project.organisationName = projectDetails['Grantee Full Name']
+        try {
+            def startDate = projectDetails['Start']
+            if (startDate) {
+                project.plannedStartDate = convertDate(startDate)
+            }
+        }
+        catch (Exception e) {
+            errors.add("Unable to parse Finish Date: ${endDate}")
+        }
+        try {
+            def endDate = projectDetails['Finish']
+            if (endDate) {
+                project.plannedEndDate = convertDate(endDate);
+            }
+        }
+        catch (Exception e) {
+            errors.add("Unable to parse Finish Date: ${endDate}")
+        }
 
         if (grantIds.contains(project.grantId)) {
             errors.add("Duplicate Grant ID detected: '$project.grantId'")
-        }
-
-        if (!project.name) {
-            errors.add("No name supplied for the project.")
         }
 
         if (projectDetails['Application Location Desc']) {
@@ -121,17 +175,20 @@ class ImportService {
         }
 
         if (projectDetails['Round Name']) {
-            def roundDetails = projectDetails['Round Name'];
+            String roundDetails = projectDetails['Round Name'];
+            // For whatever reason, many/all of the caring for our country programs are coded as "C4OC 2"
+            roundDetails = roundDetails.replace("C4OC 2", "Caring for our Country 2")
+
             def program = metadataService.programsModel().programs.find{ roundDetails.startsWith(it.name)}
 
             if (!program) {
-                errors.add("'Round Name' does not match a valid program name")
+                warnings.add("'Round Name' does not match a valid program name: ${roundDetails}")
             }
             else {
                 project.associatedProgram = program.name
                 def subprogram = program.subprograms.find {roundDetails.contains(it.name)}
                 if (!subprogram) {
-                    errors.add("'Round Name' does not match a valid subprogram name")
+                    warnings.add("'Round Name' does not match a valid subprogram name: ${roundDetails}")
                 }
                 else {
                     project.associatedSubProgram = subprogram.name
@@ -143,8 +200,11 @@ class ImportService {
         return project
     }
 
-    def importProject(project) {
+    def convertDate(date) {
+        outputDateFormat.format(inputDateFormat.parse(date))
+    }
 
+    def importProject(project) {
 
         def status = [:]
 
@@ -167,7 +227,7 @@ class ImportService {
         def projectId = result.resp.projectId?:project.projectId
 
 
-        if (site && site.description) {
+        if (false && site && site.description) {
 
             // Get the project details, including any related sites.
             p = projectService.get(projectId)
@@ -204,8 +264,8 @@ class ImportService {
 
     def findProjectByGrantId(grantId) {
         // Cache projects temporarily to avoid this query.
-        def allProjects = cacheService.get(PROJECTS_CACHE_KEY) { projectService.list(true) }
-        return allProjects.find{it.grantId?.equalsIgnoreCase(grantId)}
+        def allProjects = cacheService.get(PROJECTS_CACHE_KEY) { [projects:projectService.list(true)] }
+        return allProjects.projects.find{it.grantId?.equalsIgnoreCase(grantId)}
     }
 
     def findProjectSiteByDescription(project, description) {
@@ -219,13 +279,15 @@ class ImportService {
     }
 
 
-    private def writeErrors(results, errors, String[] line) {
+    private def writeErrors(results, errors, warnings, String[] line) {
         def errorLine = []
         errorLine << errors.join('\n')
+        errorLine << warnings.join('\n')
+
         line.each{
             // The truncation is because the CSV file is slow because of the large amount of repetition in the
             // location cells in particular
-            errorLine << it.substring(0, Math.min(it.length(), 40)).replaceAll('\n', ' ').replaceAll('"', "'")
+            errorLine << it.substring(0, Math.min(it.length(), 100)).replaceAll('\n', ' ').replaceAll('"', "'")
         }
 
         results << errorLine
