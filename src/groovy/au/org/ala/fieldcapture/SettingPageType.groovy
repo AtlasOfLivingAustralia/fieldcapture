8e36abc83e4065f8230322ad0d4386f410f7eac5
nickdos <nickdos@gmail.com>
2013-12-04 22:42:00 +0000

    Added missing file from previous commit.
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@1029 63d11371-6d71-872f-44d1-802e8bf8a75e

---
+++ src/groovy/au/org/ala/fieldcapture/SettingPageType.groovy
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2013 Atlas of Living Australia
+ * All Rights Reserved.
+ *
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
+package au.org.ala.fieldcapture
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
+ */
+enum SettingPageType {
+    ABOUT ("about","About Page Content","about.page.text"),
+    FOOTER ("footer","Page Footer Content","page.footer.text"),
+    ANNOUNCEMENT ("announcement","Announcement Page Content","announcement.page.text")
+
+    String name
+    String title
+    String key
+
+    public SettingPageType(name, title, key) {
+        this.name = name
+        this.title = title
+        this.key = key
+    }
+
+    public static SettingPageType getForName(String name) {
+        for(SettingPageType s : values()){
+            if (s.name == name){
+                return s
+            }
+        }
+    }
+
+    public static SettingPageType getForKey(String key) {
+        for(SettingPageType s : values()){
+            if (s.key == key){
+                return s
+            }
+        }
+    }
+}
