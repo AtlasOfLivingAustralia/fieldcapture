71ea3e0f1f0806b42a4225c5895851aa7d7542f6
chrisala <chris.godwin.ala@gmail.com>
2014-03-17 03:29:19 +0000

    Added a UI block when changing an activity progress to started or finished to prevent a slow page load from causing confusion.
    git-svn-id: http://ala-fieldcapture.googlecode.com/svn/trunk/FieldCapture@1413 63d11371-6d71-872f-44d1-802e8bf8a75e

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -184,3 +184,16 @@ function showAlert(message, alerttype, target) {
         $("#alertdiv").remove();
     }, 5000);
 }
+
+function blockUIWithMessage(message) {
+    $.blockUI({ message: message,
+        css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: .5,
+            color: '#fff'
+        } });
+}
