d79b31c0ccbdd071e7286e4d20cf14cbb0868727
chrisala <chris.godwin.ala@gmail.com>
2014-10-08 15:40:29 +1100

    Added the release plugin for the travis build.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -89,5 +89,8 @@ grails.project.dependency.resolution = {
         test (":spock:0.7") {
             exclude "spock-grails-support"
         }
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+            export = false
+        }
     }
 }
