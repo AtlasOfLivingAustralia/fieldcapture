7227c76ee2a94e0f60a81484a121676f36e6af51
chrisala <chris.godwin.ala@gmail.com>
2015-05-29 17:09:28 +1000

    Fixed broken test.

--- test/unit/au/org/ala/merit/OrganisationServiceSpec.groovy
+++ test/unit/au/org/ala/merit/OrganisationServiceSpec.groovy
@@ -3,8 +3,10 @@ package au.org.ala.merit
 import au.org.ala.fieldcapture.ActivityService
 import au.org.ala.fieldcapture.DateUtils
 import au.org.ala.fieldcapture.WebService
+import grails.converters.JSON
 import grails.test.mixin.TestFor
 import groovy.time.TimeCategory
+import org.joda.time.Months
 import org.joda.time.Period
 import spock.lang.Specification
 
@@ -22,6 +24,7 @@ class OrganisationServiceSpec extends Specification {
 		service.webService = webService
 	}
 
+
 	def cleanup() {
 	}
 
@@ -31,7 +34,7 @@ class OrganisationServiceSpec extends Specification {
 		service.findActivitiesForOrganisation(organisationWithProjects(), ['t1', 't2'])
 
 		then:
-		1 * activityService.search([type:['t1', 't2'], projectId:['1', '2', '3'], plannedStartDate:"2014-01-01T00:00:00+0000", plannedEndDate:'2016-02-28T00:00:00+0000'])
+		1 * activityService.search([type:['t1', 't2'], projectId:['1', '2', '3'], dateProperty:'plannedEndDate', startDate:"2014-01-01T00:00:00+0000", endDate:'2016-02-28T00:00:00+0000'])
 	}
 
 	void "organisational level reports should be grouped based on report type and reporting period"() {
@@ -49,12 +52,12 @@ class OrganisationServiceSpec extends Specification {
 		when:
 		def reports = service.getReportsForOrganisation(organisation, reportConf)
 		def reportsByType = reports.groupBy{it.type}
-		then: "2 years, 3 months of monthly reports"
-		reportsByType['t1'].size() == 27
+
+        then: "2 years, 3 months of monthly reports, the first activity finishes on the 1st of Feb so isn't reportable until Feb"
+		reportsByType['t1'].size() == 26
 
 		and: "9 quarterly reports"
 		reportsByType['t2'].size() == 9
-
 	}
 
 	def organisationActivities(organisation) {
@@ -73,21 +76,22 @@ class OrganisationServiceSpec extends Specification {
 		def reports = []
 		use(TimeCategory) {
 			def reportConf = [
-					[type: 't1', period: 1.months],
-					[type: 't2', period: 3.months]]
+					[type: 't1', period: Months.months(1)],
+					[type: 't2', period: Months.months(3)]]
 
-			def projectEndDate = DateUtils.parse(project.plannedEndDate).toDate()
+			def projectEndDate = DateUtils.parse(project.plannedEndDate)
 
 			reportConf.each { conf ->
-				def date = DateUtils.parse(project.plannedStartDate).toDate()
+				def date = DateUtils.parse(project.plannedStartDate)
 
-				while (date.before(projectEndDate)) {
+				while (date.isBefore(projectEndDate)) {
 
-					def endDate = date + conf.period
-					def periodDescription = endDate.format('MMM yyyy')
+					def endDate = date.plus(conf.period)
+					def periodDescription = endDate.toDate().format('MMM yyyy')
+
 					// brings the end date into the previous month
-					if (conf.period > 1.months) {
-						periodDescription = date.format('MMM') + '-' + periodDescription
+					if (conf.period.isGreaterThan(Months.months(1))) {
+						periodDescription = date.toDate().format('MMM') + '-' + periodDescription
 					}
 
 					def description = conf.type + ' - ' + periodDescription
