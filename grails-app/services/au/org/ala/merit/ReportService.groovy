5d9c0ed2bb1049a00914c861d1fe8730932e1c51
chrisala <chris.godwin.ala@gmail.com>
2015-10-16 14:29:51 +1100

    Setup a template for designers #300

--- grails-app/services/au/org/ala/merit/ReportService.groovy
+++ grails-app/services/au/org/ala/merit/ReportService.groovy
@@ -12,6 +12,7 @@ class ReportService {
     def userService
     def projectService
     def authService
+    def searchService
 
     /**
      * This method supports automatically creating reporting activities for a project that re-occur at defined intervals.
@@ -136,6 +137,59 @@ class ReportService {
         if (reports.resp && !reports.error) {
             return reports.resp.projectReports.groupBy{it.projectId}
         }
+
+    }
+
+    def getHighlightedStatistics() {
+
+        //"Number of projects addressing threatened species"
+        //searchService.search([fq:'meriPlanAssetFacet:Threatened+Species'])
+
+
+        //fq:"associatedSubProgram:Reef Rescue 2013/14"
+        //score:"Total No. of farming entities adopting sustainable practice change"
+
+//        Reef
+//
+//        Weight of marine debris removed
+//        (Pest Management) Number of Crown of Thorn Starfish removed
+//        Length of fences erected
+//        Additional info-graphics for Future projects (Reef Trust phase II, III and IV)
+//
+//        Total Erosion area treated
+//        Indigenous
+//
+//        No employed and no of on country visits for Indigenous
+//        National Landcare Programme
+//
+//        $XXX and no. Project Sustainable ag - If we tell you which budget line is sustainable agriculture could we aggregate this figure?  custom.details.budget.rows.shortLabel:"MERI & Admin"
+//                $XXX and no. Projects - to support Indigenous projects (using Asset addressed)
+//        $XXX and no. Projects - that help protect threatened species and their habitat (using Asset addressed)
+//        $XXX and no. Projects - that support the health of World Heritage Area (using Asset addressed).
+//        Pest animals area managed – target for now, moving to delivered down the track?
+//                Weeds treated by area, moving to delivered down the track?
+//                20MT-number of trees (>2m) planted, also ability to modify this to number of trees surviving as program matures. Display – progress against target of 20 million
+//        All programmes:
+//
+//        Ha of weeds removed
+//        All programmes: Area treated for felius catus
+//        NLP:
+//
+//        Ha weed treatment by main activity partner ‘Local Landcare, ‘Friends of’, community, or farmer groups
+//        No of volunteers participating in project activities
+//        Green Army:
+//
+//        No. of Participants who commenced projects
+//        Green Army: No of teams (projects), active and complete
+        [
+                [config:'1', programme:'National Landcare Programme', text:'volunteers participating', value:'234', units:''],
+                [config:'2', programme:'Green Army', text:'of debris removed', value:'40,149', units:'m<super>2</super>'],
+                [config:'3', programme:'Green Army', text:'of weed treatment', value:'5,486', units:'Ha'],
+                [config:'4', programme:'Green Army', text:'plants planted', value:'101,641', units:''],
+                [config:'5', programme:'Green Army', text:'active & complete teams', value:'128', units:''],
+                [config:'6', programme:'20 Million Trees', text:'of 20 million trees contracted', value:'1,123,412', units:'']
+        ]
+
 
     }
 }
---
+++ grails-app/views/report/_statistic.gsp
@@ -0,0 +1,5 @@
+<div class="stat stat-${config}">
+    <div class="stat-title">${programme}</div>
+    <img class="stat-image"/><span class="stat-value">${value}</span><span class="stat-units">${units}</span>
+    <div class="stat-text">${text}</div>
+</div>
