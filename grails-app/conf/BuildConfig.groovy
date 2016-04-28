aaed27e5db2b6646f750aa85c6644dfece492073
chrisala <chrisala@users.noreply.github.com>
2016-04-28 12:05:45 +1000

    Update BuildConfig.groovy

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -75,7 +75,7 @@ grails.project.dependency.resolution = {
         //test ":karma-test-runner:0.2.4"
 
         if (Environment.current != Environment.DEVELOPMENT) {
-            compile ":fieldcapture-plugin:1.6.3"
+            compile ":fieldcapture-plugin:1.7-SNAPSHOT"
             test ":fieldcapture-test:0.1-SNAPSHOT"
         }
 
