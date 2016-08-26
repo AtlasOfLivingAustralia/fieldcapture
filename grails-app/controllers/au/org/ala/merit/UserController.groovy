d65046a3b72c0621775954a875d4c82b656aa223
chrisala <chris.godwin.ala@gmail.com>
2016-08-26 16:56:40 +1000

    Implements #719

--- grails-app/controllers/au/org/ala/merit/UserController.groovy
+++ grails-app/controllers/au/org/ala/merit/UserController.groovy
@@ -14,6 +14,7 @@ class UserController extends au.org.ala.fieldcapture.UserController {
         def userData = super.assembleUserData(user)
         def reportsByProject = reportService.findReportsForUser(user.userId)
 
+        userData.memberProjects = userData.memberProjects.findAll{it.project.isMERIT == true}
         def projects = userData.memberProjects
         projects.each { project ->
             project.project.reports = reportsByProject[project.project.projectId]? new JSONArray(reportsByProject[project.project.projectId]):new JSONArray()
