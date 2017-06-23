e75f41c4bb20d0a500a20b0b7e6c836891a4d9fd
chrisala <chris.godwin.ala@gmail.com>
2017-06-23 16:08:06 +1000

    Bumped plugin version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -85,7 +85,7 @@ grails.project.dependency.resolution = {
         //test ":karma-test-runner:0.2.4"
 
         if (Environment.current != Environment.DEVELOPMENT) {
-            compile ":fieldcapture-plugin:1.15"
+            compile ":fieldcapture-plugin:1.16-SNAPSHOT"
             test ":fieldcapture-test:0.1-SNAPSHOT"
         }
 
