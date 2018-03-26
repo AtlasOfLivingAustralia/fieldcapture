0561c7d0983633bde827cef7c41c1d7d6bde4671
chrisala <chrisala@users.noreply.github.com>
2018-03-26 11:58:44 +1100

    Plugin v0.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -120,7 +120,7 @@ grails.project.dependency.resolution = {
         compile ':cookie:1.4'
 
         if (Environment.current != Environment.DEVELOPMENT) {
-            compile ":ecodata-client-plugin:0.6-SNAPSHOT"
+            compile ":ecodata-client-plugin:0.6"
             test ":fieldcapture-test:0.1-SNAPSHOT"
         }
 
