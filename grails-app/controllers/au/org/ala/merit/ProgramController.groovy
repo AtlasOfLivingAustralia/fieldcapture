c93fa8a93b041738f7a0bda005db39bb11c47904
chrisala <chris.godwin.ala@gmail.com>
2018-06-20 13:29:50 +1000

    Program permissions fixes #1343

--- grails-app/controllers/au/org/ala/merit/ProgramController.groovy
+++ grails-app/controllers/au/org/ala/merit/ProgramController.groovy
@@ -27,7 +27,7 @@ class ProgramController {
             def roles = roleService.getRoles()
             // Get dashboard information for the response.
 
-            List members = userService.getMembersOfProgram(id).members ?: [:]
+            List members = userService.getMembersOfProgram(id).members ?: []
             def user = userService.getUser()
             def userId = user?.userId
 
