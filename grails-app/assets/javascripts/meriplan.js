9a5e84c6e879c9e417b30750627a0d89ede22e6b
Chris <chris.godwin.ala@gmail.com>
2018-11-27 14:17:39 +1100

    Attempt at #1712

--- grails-app/assets/javascripts/meriplan.js
+++ grails-app/assets/javascripts/meriplan.js
@@ -16,17 +16,34 @@ function MERIPlan(project, config) {
    if (config.meriStorageKey) {
        var savedProjectCustomDetails = amplify.store(config.meriStorageKey);
        if (savedProjectCustomDetails) {
-          var restored = JSON.parse(savedProjectCustomDetails);
-          $('#restoredData').show();
-          if (restored.custom) {
-             project.custom.details = restored.custom.details;
-          }
-          if (restored.outputTargets) {
-              project.outputTargets = restored.outputTargets;
-          }
-          if (restored.risks) {
-              project.risks = restored.risks;
-          }
+           var serverUpdate = project.custom.details.lastUpdated;
+           var restored = JSON.parse(savedProjectCustomDetails);
+           var userUpdate;
+           $('#restoredData').show();
+           if (restored.custom) {
+               if (restored.custom.details) {
+                   userUpdate = restored.custom.details.lastUpdated;
+               }
+               project.custom.details = restored.custom.details;
+           }
+           if (restored.outputTargets) {
+               project.outputTargets = restored.outputTargets;
+           }
+           if (restored.risks) {
+               project.risks = restored.risks;
+           }
+
+
+           var message = "<span class='label label-warning'>Important</span><p>You have unsaved changes on this page.</p>";
+           if (userUpdate && serverUpdate) {
+               var saved = moment(userUpdate);
+               message += "<p>Your unsaved changes were made on <b>"+saved.format("LLLL")+"</b></p><p>The changes we loaded from the server when this page was refreshed were made at <b>"+moment(serverUpdate).format("LLLL")+"</b></p>";
+           }
+           message += "<p>Please review the changes then press the 'Save changes' button at the bottom of the page if you want to keep your unsaved changes or the 'Cancel' button if you want to discard your changes.</p>";
+
+           bootbox.alert(message);
+
+
        }
    }
    var disableFlag = ko.observable(false);
