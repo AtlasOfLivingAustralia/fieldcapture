cff9007135289e6ef40960fbdb80c78f86c17f4c
Chris <chris.godwin.ala@gmail.com>
2019-03-18 14:14:19 +1100

    ecodata-client-plugin 1.3.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -144,7 +144,7 @@ grails.project.dependency.resolution = {
         test 'org.grails.plugins:clover:4.3.0'
 
         if (Environment.current != Environment.DEVELOPMENT) {
-            compile ":ecodata-client-plugin:1.3.1-SNAPSHOT"
+            compile ":ecodata-client-plugin:1.3.1"
         }
 
     }
