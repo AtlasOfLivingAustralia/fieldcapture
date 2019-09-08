95d960c944daa16c4368d7a2d8dad65b843273b7
Chris <chris.godwin.ala@gmail.com>
2019-09-09 09:30:26 +1000

    Attempting to run functional tests in travis.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,16 +1,11 @@
 import au.org.ala.merit.SessionLogger
 import au.org.ala.merit.SettingService
-import au.org.ala.merit.hub.HubSettings
 import grails.converters.JSON
-import groovy.json.JsonSlurper
 import net.sf.json.JSONNull
 import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
 import org.joda.time.format.DateTimeFormatter
-
-import static au.org.ala.merit.ScheduledJobContext.withUser
-
 
 class BootStrap {
 
