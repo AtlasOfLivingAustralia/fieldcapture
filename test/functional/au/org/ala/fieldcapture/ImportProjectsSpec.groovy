3a61dab1311d1b6d11fd960d37b26ed43d0935c7
Chris <chris.godwin.ala@gmail.com>
2019-09-17 13:47:52 +1000

    Fixing a race condition in functional tests.

--- test/functional/au/org/ala/fieldcapture/ImportProjectsSpec.groovy
+++ test/functional/au/org/ala/fieldcapture/ImportProjectsSpec.groovy
@@ -20,7 +20,7 @@ class ImportProjectsSpec extends StubbedCasSpec {
 
 
         then: "The projects are validated and the validation results are displayed"
-        waitFor{projectResults().size() == 2}
+        waitFor{loadComplete()}
 
         and: "The data is relevant to the projects loaded"
         List rows = projectResults()
--- test/functional/pages/ProjectImport.groovy
+++ test/functional/pages/ProjectImport.groovy
@@ -38,6 +38,7 @@ class ProjectImport extends Page {
 
     boolean loadComplete() {
         // The page will update to say "All x projects processed." or "Import complete" when the load has finished
-        progressSummary.text().startsWith("All") || progressSummary.text().startsWith("Import complete")
+        progressSummary.text().startsWith("All") || progressSummary.text().startsWith("Import complete") || progressSummary.text().startsWith('Processed ')
+
     }
 }
