d215b0d7d6565ea5589dc9a5d8b85d3cc678e936
Chris <chris.godwin.ala@gmail.com>
2019-09-18 11:40:57 +1000

    Trying to fix functional test race condition.

--- test/functional/pages/ProjectImport.groovy
+++ test/functional/pages/ProjectImport.groovy
@@ -36,9 +36,12 @@ class ProjectImport extends Page {
         rows
     }
 
+    boolean validateComplete() {
+        progressSummary.text().startsWith('Processed ')
+    }
     boolean loadComplete() {
         // The page will update to say "All x projects processed." or "Import complete" when the load has finished
-        progressSummary.text().startsWith("All") || progressSummary.text().startsWith("Import complete") || progressSummary.text().startsWith('Processed ')
+        progressSummary.text().startsWith("All") || progressSummary.text().startsWith("Import complete")
 
     }
 }
