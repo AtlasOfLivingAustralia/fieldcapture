e9c62cc955ae20ef9c2e04d50408505549f5db59
Chris <chris.godwin.ala@gmail.com>
2020-06-16 10:39:54 +1000

    ecodata-client-plugin 1.14.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -154,7 +154,7 @@ grails.project.dependency.resolution = {
         test "org.grails.plugins:geb:1.0"
 
         if (Environment.current != Environment.DEVELOPMENT) {
-            compile (":ecodata-client-plugin:1.14.1s")
+            compile (":ecodata-client-plugin:1.14.1")
         }
 
     }
