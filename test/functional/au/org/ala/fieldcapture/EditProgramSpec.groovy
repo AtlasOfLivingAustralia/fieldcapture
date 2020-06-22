a55a13a89971d45afb919d631f997cb5d6f74ac5
Thakur Adhikari <adh010@csiro.au>
2020-06-22 13:37:34 +1000

    #2047 fixed NPE and added to support functional test

--- test/functional/au/org/ala/fieldcapture/EditProgramSpec.groovy
+++ test/functional/au/org/ala/fieldcapture/EditProgramSpec.groovy
@@ -1,6 +1,7 @@
 package au.org.ala.fieldcapture
 
 import pages.EditProgram
+import pages.RLPEditPageWithNoParent
 import pages.RLPProgramPage
 
 class EditProgramSpec extends StubbedCasSpec {
@@ -13,7 +14,7 @@ class EditProgramSpec extends StubbedCasSpec {
         logout(browser)
     }
 
-    def "As a user with admin permissions, I can edit a program"() {
+    def "As a user with admin permissions, I can edit a program with parent id"() {
         setup: "log in as userId=1 who is a program admin for the program with programId=test_program"
         login([userId:'1', role:"ROLE_FC_ADMIN", email:'fc-admin@nowhere.com', firstName: "FC", lastName:'Admin'], browser)
 
@@ -45,4 +46,35 @@ class EditProgramSpec extends StubbedCasSpec {
         "The program details have been updated"
     }
 
+    def "As a user with admin permissions, I can edit a program with No parent"() {
+        setup: "log in as userId=1 who is a program admin for the program with programId=test_programId"
+        login([userId:'1', role:"ROLE_FC_ADMIN", email:'fc-admin@nowhere.com', firstName: "FC", lastName:'Admin'], browser)
+
+        when:
+        to RLPEditPageWithNoParent
+
+        and:
+        edit()
+
+        then:
+        waitFor { at EditProgram }
+
+        when:
+        details.newParentProgramId.click()
+        details.newParentProgramId.find("span").find{it.text() == "No Parent"}.click()
+        details.description= "Testing"
+        details.url = "http://ala.org.au"
+        details.save()
+
+
+        then:
+        at RLPEditPageWithNoParent
+        overviewTab.click()
+        overviewTab.displayed
+        description.text() == "Testing"
+        visitUs.text() == "http://ala.org.au"
+
+        and:
+        "The program details have been updated"
+    }
 }
