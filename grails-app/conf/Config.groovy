6f1102cce40c5c294a107fc29f14a23dcc562c5a
Chris <chris.godwin.ala@gmail.com>
2021-04-15 09:08:23 +1000

    Set development to use grails3/4 ecodata paths #2208

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -320,11 +320,11 @@ environments {
         layout.skin = "nrm"
         security.cas.appServerName = serverName
         security.cas.contextPath = "/" + appName
-        ecodata.baseUrl = 'http://devt.ala.org.au:8080/ecodata/ws/'
+        ecodata.baseUrl = 'http://devt.ala.org.au:8080/ws/'
         upload.images.url = grails.serverURL+'/image/'
         emailFilter = /[A-Z0-9._%-]+@csiro\.au|chris\.godwin\.ala@gmail.com/
         logging.dir = '.'
-        ecodata.service.url = 'http://devt.ala.org.au:8080/ecodata/ws'
+        ecodata.service.url = 'http://devt.ala.org.au:8080/ws'
         espSupportEmail='ESPmonitoring@environment.gov.au'
     }
     test {
