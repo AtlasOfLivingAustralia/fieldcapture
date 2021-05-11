35fcb995e3217aeb06ce69895bf2b49dd1562f03
Thakur Adhikari <adh010@csiro.au>
2021-05-11 13:01:41 +1000

    Functional Test #2222

--- test/functional/pages/SiteIndexPage.groovy
+++ test/functional/pages/SiteIndexPage.groovy
@@ -5,7 +5,7 @@ import geb.Page
 class SiteIndexPage  extends Page{
     static url = 'site/index'
 
-    static at = { title.endsWith('Field Capture')}
+    static at = { title.endsWith('MERIT')}
 
     static content = {
         name {$(".siteName")}
