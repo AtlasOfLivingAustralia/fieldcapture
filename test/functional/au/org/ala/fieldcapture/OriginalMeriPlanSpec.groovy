a1bf3205b1f42f98c868402cfda8b618a0f0288d
Thakur Adhikari <adh010@csiro.au>
2021-05-14 10:57:13 +1000

    #2222 Update Functional Test

--- test/functional/au/org/ala/fieldcapture/OriginalMeriPlanSpec.groovy
+++ test/functional/au/org/ala/fieldcapture/OriginalMeriPlanSpec.groovy
@@ -39,30 +39,42 @@ class OriginalMeriPlanSpec extends StubbedCasSpec {
 
         meriPlan.save()
 
-        def previousLoad = getAtCheckTime()
-        to RlpProjectPage, projectId
-        waitFor { getAtCheckTime() > previousLoad }
-        meriPlan = openMeriPlanEditTab()
+        then:
+        waitFor {
+            meriPlan.objectivesAndAssets[0].outcome.value() == "Objective 1"
+            meriPlan.objectivesAndAssets[0].assets.value() == ['Threatened Species'];
+            meriPlan.objectivesAndAssets[1].outcome.value() == "Objective 2"
+            meriPlan.objectivesAndAssets[1].assets.value() == ['Threatened Species'];
+        }
+    }
+
+    def "The MERI Plan objectives remove and assets can be manipulated and saved"() {
+
+        setup:
+        String projectId = 'grants_project'
+        login([userId: '2', role: "ROLE_FC_OFFICER", email: 'admin@nowhere.com', firstName: "MERIT", lastName: 'FC_ADMIN'], browser)
+
+        when:
+        to RlpProjectPage, projectId // Note we are using the RlpProjectPage here despite it being the original page template because it works and has the content we need
 
         then:
-        meriPlan.objectivesAndAssets[0].outcome.value() == "Objective 1"
-        meriPlan.objectivesAndAssets[0].assets.value() == ['Threatened Species'];
-        meriPlan.objectivesAndAssets[1].outcome.value() == "Objective 2"
-        meriPlan.objectivesAndAssets[1].assets.value() == ['Threatened Species'];
+        waitFor { at RlpProjectPage }
+
+        when:
+        def meriPlan = openMeriPlanEditTab()
+
+        then:
+        meriPlan != null
 
         when:
         meriPlan.objectivesAndAssets[1].remove()
         meriPlan.save()
 
-        previousLoad = getAtCheckTime()
-        to RlpProjectPage, projectId
-        waitFor { getAtCheckTime() > previousLoad }
-        meriPlan = openMeriPlanEditTab()
-
         then:
-        meriPlan.objectivesAndAssets.size() == 1
-        meriPlan.objectivesAndAssets[0].outcome.value() == "Objective 1"
-        meriPlan.objectivesAndAssets[0].assets.value() == ['Threatened Species'];
-
+        waitFor {
+            meriPlan.objectivesAndAssets.size() == 1
+            meriPlan.objectivesAndAssets[0].outcome.value() == "Objective 1"
+            meriPlan.objectivesAndAssets[0].assets.value() == ['Threatened Species'];
+        }
     }
 }
