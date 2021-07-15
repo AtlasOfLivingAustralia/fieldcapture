0dfa0d0357f4b33b8732a57d11165d8f51163883
Chris <chris.godwin.ala@gmail.com>
2021-07-15 10:57:01 +1000

    Rolled back some changes from recent commit #2209

--- grails-app/assets/javascripts/base-bs4.js
+++ grails-app/assets/javascripts/base-bs4.js
@@ -1,4 +1,4 @@
 //= require html5/html5
-//= require webjars/jquery/1.12.4/jquery.js
+//= require jquery/3.5.1/jquery-3.5.1.min.js
 //= require jquery-cookie/jquery.cookie
 //= require bootstrap4/4.6.0/js/bootstrap.bundle
