11a3c5738ca1c505e15b2a928d80cd29dbff4241
Chris <chris.godwin.ala@gmail.com>
2021-07-19 11:59:54 +1000

    Fixed tests for #2290

--- test/unit/au/org/ala/merit/command/Reef2050PlanActionReportSummaryCommandSpec.groovy
+++ test/unit/au/org/ala/merit/command/Reef2050PlanActionReportSummaryCommandSpec.groovy
@@ -29,7 +29,7 @@ class Reef2050PlanActionReportSummaryCommandSpec extends Specification {
         command.approvedActivitiesOnly == true
     }
 
-    def "The three legacy reports will always be returned"() {
+    def "The 3 legacy report and the 2020/2021 report will always be returned"() {
 
         when:
         List reports = command.reportSummary()
@@ -37,7 +37,7 @@ class Reef2050PlanActionReportSummaryCommandSpec extends Specification {
         then:
         1 * activityService.search( [type    : Reef2050PlanActionReportConfig.REEF_2050_PLAN_ACTION_REPORTING_2018_ACTIVITY_TYPE,
                                      progress: ActivityService.PROGRESS_FINISHED, publicationStatus: ReportService.REPORT_APPROVED]) >> [reports:[]]
-        reports.size() == 3
+        reports.size() == 4
     }
 
     def "The new format reports will be returned if there is appropriate activity data for them"() {
@@ -47,7 +47,7 @@ class Reef2050PlanActionReportSummaryCommandSpec extends Specification {
         then:
         1 * activityService.search( [type    : Reef2050PlanActionReportConfig.REEF_2050_PLAN_ACTION_REPORTING_2018_ACTIVITY_TYPE,
                                      progress: ActivityService.PROGRESS_FINISHED, publicationStatus: ReportService.REPORT_APPROVED]) >> [resp:[activities:[[plannedEndDate:'2018-07-01T00:00:00Z']]]]
-        reports.size() == 4
+        reports.size() == 5
     }
 
 
