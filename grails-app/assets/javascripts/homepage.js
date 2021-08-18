f005057b69b3e16ad3ea6a7adef6dd15dd14ff8e
Thakur Adhikari <adh010@csiro.au>
2021-08-18 22:44:37 +1000

    #2338 Progress Commit

--- grails-app/assets/javascripts/homepage.js
+++ grails-app/assets/javascripts/homepage.js
@@ -1,5 +1,5 @@
-//= require pdfmake/0.1.18/pdfmake.min.js
-//= require pdfmake/0.1.18/vfs_fonts.js
+//= require pdfmake/pdfmake.js
+//= require pdfmake/vfs_fonts.js
 //= require wms
 //= require mapWithFeatures.js
 //= require slider-pro/js/jquery.sliderPro.js
