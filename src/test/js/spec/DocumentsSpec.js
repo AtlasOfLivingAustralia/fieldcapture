7ecfef67dd0ef8634db772f3899f3fe56f744ff8
Thakur Adhikari <adh010@csiro.au>
2021-08-31 08:26:26 +1000

    #2346 progress commit

--- src/test/js/spec/DocumentsSpec.js
+++ src/test/js/spec/DocumentsSpec.js
@@ -4,6 +4,11 @@ describe("The documents contains view models for working with documents", functi
         window.fcConfig = {
             imageLocation:'/'
         };
+
+        // $.fn.fileupload = function () {
+        //         console.log("Retuning on")
+        // }
+
     });
     afterAll(function() {
         delete window.fcConfig;
@@ -31,5 +36,27 @@ describe("The documents contains view models for working with documents", functi
 
     });
 
-
+    // function ajax_response() {
+    //     var deferred = $.Deferred().resolve();
+    //     return deferred.promise();
+    // }
+    // it("should able to close the document modal without any error", function () {
+    //     var options = {
+    //         documentUpdateUrl: "document/documentUpdate",
+    //         modalSelector: "#attachDocument",
+    //         report: {}
+    //     }
+    //
+    //     var editableDocumentsViewModel = new EditableDocumentsViewModel(options);
+    //
+    //     var showModal = new showDocumentAttachInModal(options.documentUpdateUrl, {reportId: "234"}, options.modalSelector, options.modalSelector, "#preview")
+    //     editableDocumentsViewModel.attachDocument()
+    //     spyOn($(options.modalSelector), 'fileupload').and.returnValue(
+    //         ajax_response()
+    //     )
+    // failing in the fileupload.
+    //     expect("showDocumentAttachInModal"["attachViewModelToFileUpload"]).toHaveBeenCalled();
+    //
+    // //    expect(editableDocumentsViewModel.size() === 0)
+    // })
 });
