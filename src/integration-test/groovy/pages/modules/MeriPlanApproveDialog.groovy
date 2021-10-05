59e0daf24cdec233d439ab926dcb81600fafaa83
Chris <chris.godwin.ala@gmail.com>
2021-10-05 15:32:31 +1100

    Attempting to fix a race in MERIPlanSpec #2395

--- src/integration-test/groovy/pages/modules/MeriPlanApproveDialog.groovy
+++ src/integration-test/groovy/pages/modules/MeriPlanApproveDialog.groovy
@@ -1,6 +1,7 @@
 package pages.modules
 
 import geb.Module
+import geb.module.FormElement
 
 class MeriPlanApproveDialog extends Module {
     static content = {
@@ -9,5 +10,13 @@ class MeriPlanApproveDialog extends Module {
         comment { $('#meri-plan-approval-reason') }
 
         approvePlanButton{ $('button[data-bind*=referenceDocument]') }
+    }
+
+    def approve() {
+        // Click the header to move focus away from any input fields to trigger the knockout update
+        // to enable the button if necessary.
+        $('.modal-header h4').click()
+        waitFor { approvePlanButton.module(FormElement).enabled }
+        approvePlanButton.click()
     }
 }
