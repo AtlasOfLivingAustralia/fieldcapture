1e15f7e426da61d92716042eb64c424f127fe769
Chris <chris.godwin.ala@gmail.com>
2021-11-24 07:40:30 +1100

    Removed reliance on previous test state and tidy up #2443

--- src/integration-test/groovy/au/org/ala/fieldcapture/ProjectExplorerCrossSiteInjectionSpec.groovy
+++ src/integration-test/groovy/au/org/ala/fieldcapture/ProjectExplorerCrossSiteInjectionSpec.groovy
@@ -23,32 +23,11 @@ class ProjectExplorerCrossSiteInjectionSpec  extends StubbedCasSpec{
         then: "The downloads accordion is not visible to unauthenticated users"
         downloadsToggle.empty == true
 
-        when: "collapse the map section"
-
-        waitFor {
-            map.displayed
-        }
-        mapToggle.click()
-
-        then:
-        waitFor { map.displayed == false }
-
         when: "expand the projects section"
-        def expectedProjects = new HashSet()
-        expectedProjects.add("Project Script Injection <script>alert('Test')</script>")
-        projectsToggle.click()
+        displayProjectList()
 
         then:
-        waitFor 20, {
-
-            to ProjectExplorer
-        }
-        waitFor(10) {projects.displayed}
-        def projectList = projects
-
-        and:
-        waitFor(20){projectList}
-        projectList.name[0] =="Project Script Injection <script>alert('Test')</script>"
-        projectList.managementUnit[0] == 'test mu <script>alert("MU")</script>'
+        projects.name[0] =="Project Script Injection <script>alert('Test')</script>"
+        projects.managementUnit[0] == 'test mu <script>alert("MU")</script>'
     }
 }
