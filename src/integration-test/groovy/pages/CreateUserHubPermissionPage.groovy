a7b4bbca0e08a3a79b415ecc4260e51d2111444b
Chris <chris.godwin.ala@gmail.com>
2021-11-30 07:37:56 +1100

    Fixed at check after page title was changed #2419

--- src/integration-test/groovy/pages/CreateUserHubPermissionPage.groovy
+++ src/integration-test/groovy/pages/CreateUserHubPermissionPage.groovy
@@ -5,7 +5,7 @@ import pages.modules.PermissionsAdminModule
 
 class CreateUserHubPermissionPage extends Page {
     static url = "admin/createUserHubPermission"
-    static at = { title.startsWith("User Permission for MERIT")}
+    static at = { title.startsWith("User Permissions for MERIT")}
 
 
     static content = {
