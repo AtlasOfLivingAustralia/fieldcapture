410d825d7052915d9205b0231ae8c8cc063f24f3
Chris <chris.godwin.ala@gmail.com>
2021-12-06 13:29:24 +1100

    More permissions tests #2448

--- src/integration-test/groovy/au/org/ala/fieldcapture/ProjectPermissionSpec.groovy
+++ src/integration-test/groovy/au/org/ala/fieldcapture/ProjectPermissionSpec.groovy
@@ -1,5 +1,6 @@
 package au.org.ala.fieldcapture
 
+import org.openqa.selenium.ElementNotInteractableException
 import pages.RlpProjectPage
 
 class ProjectPermissionSpec extends StubbedCasSpec {
@@ -11,8 +12,7 @@ class ProjectPermissionSpec extends StubbedCasSpec {
         logout(browser)
     }
 
-
-    def "the project details are displayed correctly on the overview tab"() {
+    def "a grant manager can add and modify user roles for a project"() {
 
         setup:
         String projectId = '1'
@@ -44,6 +44,49 @@ class ProjectPermissionSpec extends StubbedCasSpec {
         then:
         waitFor {
             adminContent.projectAccess.findPermissionForUser('2').roleText == "Editor"
+        }
+
+        when: "We delete the new permission"
+        adminContent.projectAccess.findPermissionForUser('2').remove()
+
+        and: "Confirm we want to remove the role"
+        okBootbox()
+
+        then:
+        waitFor {
+            adminContent.projectAccess.permissions.size() == 3
+        }
+        !adminContent.projectAccess.findPermissionForUser('2')
+
+    }
+
+    def "an admin cannot add a user as a grant manager, but change change an editor to an admin"() {
+
+        setup:
+        String projectId = '1'
+        loginAsUser('1', browser)
+
+        when:
+        to RlpProjectPage, projectId
+        openAdminTab()
+        adminContent.openProjectAccess()
+
+        then:
+        adminContent.projectAccess.permissions.size() == 3
+
+        when: "We try and add a grant manager (user with id = 1001) as a grant manager to the project"
+        adminContent.projectAccess.addPermission("user1001@user.com", "caseManager")
+
+        then: "we cannot because the 'Grant Manager' option is disabled"
+        thrown(ElementNotInteractableException)
+
+        when: "We change user 10 to an admin"
+        adminContent.projectAccess.findPermissionForUser('10').updateRole('admin')
+        okBootbox()
+
+        then:
+        waitFor {
+            adminContent.projectAccess.findPermissionForUser('10').roleText == "Admin"
         }
 
     }
