63cb74c2f631c0a9b3c655d884381682a96bbef1
Chris <chris.godwin.ala@gmail.com>
2021-12-16 13:56:10 +1100

    Update openhtml2pdf for #2462

--- grails-app/services/au/org/ala/merit/PdfConverterService.groovy
+++ grails-app/services/au/org/ala/merit/PdfConverterService.groovy
@@ -1,10 +1,8 @@
 package au.org.ala.merit
 
-import com.openhtmltopdf.DOMBuilder
-import com.openhtmltopdf.log4j.Log4JXRLogger
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder
-import com.openhtmltopdf.util.XRLog
 import org.jsoup.Jsoup
+import org.jsoup.helper.W3CDom
 
 import javax.annotation.PostConstruct
 
@@ -16,19 +14,17 @@ class PdfConverterService {
     void init() {
         // PDF converter initialisation
         System.setProperty('pdfbox.fontcache', grailsApplication.config.getProperty('pdfbox.fontcache'))
-        XRLog.setLoggingEnabled(true);
-        XRLog.setLoggerImpl(new Log4JXRLogger());
     }
 
     def convertToPDF(String page, OutputStream out) {
-        PdfRendererBuilder builder = new PdfRendererBuilder();
+        PdfRendererBuilder builder = new PdfRendererBuilder()
         org.w3c.dom.Document document = html5ParseDocument(page)
         builder.withW3cDocument(document, grailsApplication.config.getProperty('grails.serverURL')).toStream(out).run()
     }
 
     private org.w3c.dom.Document html5ParseDocument(String page) throws IOException {
 
-        org.jsoup.nodes.Document doc = Jsoup.parse(page, "UTF-8");
-        return DOMBuilder.jsoup2DOM(doc);
+        org.jsoup.nodes.Document doc = Jsoup.parse(page, "UTF-8")
+        return new W3CDom().fromJsoup(doc)
     }
 }
--- src/main/groovy/au/org/ala/merit/CheckRisksAndThreatsTask.groovy
+++ src/main/groovy/au/org/ala/merit/CheckRisksAndThreatsTask.groovy
@@ -1,7 +1,6 @@
 package au.org.ala.merit
 
 import groovy.util.logging.Slf4j
-import org.apache.log4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 
