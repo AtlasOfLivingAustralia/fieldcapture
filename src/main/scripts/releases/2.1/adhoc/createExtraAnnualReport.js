b5d3ff317d49174c131fe99d9518e4d502d353c5
Chris <chris.godwin.ala@gmail.com>
2022-01-07 10:14:50 +1100

    Fixed date for ERF project. #2467

--- src/main/scripts/releases/2.1/adhoc/createExtraAnnualReport.js
+++ src/main/scripts/releases/2.1/adhoc/createExtraAnnualReport.js
@@ -7,7 +7,7 @@ var now = ISODate();
 // Change first annual report, insert new report.
 // ERF-MU42-P2
 var reportId = 'bc55d57f-62b3-4f77-82f3-97322e2c9e06'; // Report to move entirely into 20/21
-updateReportDetails(reportId, 'Annual Progress Report 2020 - 2021 for Protecting Forty-spotted Pardalote', ISODate("2021-06-24T14:00:00Z"), ISODate('2021-06-30T14:00:00Z'), userId);
+updateReportDetails(reportId, 'Annual Progress Report 2020 - 2021 for Protecting Forty-spotted Pardalote', ISODate("2021-06-27T14:00:00Z"), ISODate('2021-06-30T14:00:00Z'), userId);
 
 // Insert new report for 21/22 as a copy of 22/23
 var toCopyId = 'dbb1a61e-50af-4d8b-950a-16de74c6cb48';
