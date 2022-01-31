d406088afaba8e54359829d0a702f5d7e28d176b
salomon-j <90952854+salomon-j@users.noreply.github.com>
2022-01-31 13:02:10 +1100

    change email sender to merit@ala.org.au

--- src/main/scripts/releases/2.1/modifyMeritHubSettings.js
+++ src/main/scripts/releases/2.1/modifyMeritHubSettings.js
@@ -20,7 +20,7 @@ if (!setting) {
     }
 }
 setting.lastUpdated = now;
-setting.value = 'Dear MERIT User\n\nOur records indicate that you have not logged into MERIT (https://fieldcapture.ala.org.au/) for nearly two years.\n\nYour access will be removed 2 weeks from today, unless you take the following action:\n\nIf you wish to retain your access at your current level, all you need to do is log into MERIT within two weeks of the date of this email.\n\nIf you no longer require access, you do not need to do anything. Your access will be automatically removed two weeks from today.\n\nIf you require access at a future stage, or miss the two week deadline, please contact the MERIT Team (merit@awe.gov.au) and we can assist you.\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@awe.gov.au\nhttps://fieldcapture.ala.org.au/';
+setting.value = 'Dear MERIT User\n\nOur records indicate that you have not logged into MERIT (https://fieldcapture.ala.org.au/) for nearly two years.\n\nYour access will be removed 2 weeks from today, unless you take the following action:\n\nIf you wish to retain your access at your current level, all you need to do is log into MERIT within two weeks of the date of this email.\n\nIf you no longer require access, you do not need to do anything. Your access will be automatically removed two weeks from today.\n\nIf you require access at a future stage, or miss the two week deadline, please contact the MERIT Team (merit@ala.org.au) and we can assist you.\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@ala.org.au\nhttps://fieldcapture.ala.org.au/';
 setting.description = 'The body of the email to send to warn a user their access to MERIT will be removed if no action is taken';
 db.setting.save(setting);
 
@@ -49,7 +49,7 @@ if (!setting) {
     }
 }
 setting.lastUpdated = now;
-setting.value = 'Dear MERIT User\n\nFurther to our previous email, your access to MERIT (https://fieldcapture.ala.org.au/) has now been removed.\nIf you feel this has been done in error, or require access at a future stage, please contact the MERIT Team (merit@awe.gov.au) and we can assist you..\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@awe.gov.au\nhttps://fieldcapture.ala.org.au/';
+setting.value = 'Dear MERIT User\n\nFurther to our previous email, your access to MERIT (https://fieldcapture.ala.org.au/) has now been removed.\nIf you feel this has been done in error, or require access at a future stage, please contact the MERIT Team (merit@ala.org.au) and we can assist you..\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@ala.org.au\nhttps://fieldcapture.ala.org.au/';
 setting.description = 'The body of the email to send when access has been removed due to no login to MERIT for a specified time';
 db.setting.save(setting);
 
@@ -75,7 +75,7 @@ if (!setting) {
     }
 }
 setting.lastUpdated = now;
-setting.value = 'Dear MERIT User\n\nYour elevated access to MERIT (https://fieldcapture.ala.org.au/) has now been removed.\nIf you feel this has been done in error, or require access at a future stage, please contact the MERIT Team (merit@awe.gov.au) and we can assist you..\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@awe.gov.au\nhttps://fieldcapture.ala.org.au/';
+setting.value = 'Dear MERIT User\n\nYour elevated access to MERIT (https://fieldcapture.ala.org.au/) has now been removed.\nIf you feel this has been done in error, or require access at a future stage, please contact the MERIT Team (merit@ala.org.au) and we can assist you..\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@ala.org.au\nhttps://fieldcapture.ala.org.au/';
 setting.description = 'The subject of the email to send when a role has been removed due to it expiring';
 db.setting.save(setting);
 
@@ -101,7 +101,7 @@ if (!setting) {
     }
 }
 setting.lastUpdated = now;
-setting.value = 'Dear MERIT User\n\nYour elevated access to MERIT (https://fieldcapture.ala.org.au/) will be removed 1 month from now.\nIf you require access extension, please contact the MERIT Team (merit@awe.gov.au) and we can assist you..\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@awe.gov.au\nhttps://fieldcapture.ala.org.au/';
+setting.value = 'Dear MERIT User\n\nYour elevated access to MERIT (https://fieldcapture.ala.org.au/) will be removed 1 month from now.\nIf you require access extension, please contact the MERIT Team (merit@ala.org.au) and we can assist you..\n\nRegards\nThe MERIT Team\nDepartment of Agriculture, Water and the Environment\nmerit@ala.org.au\nhttps://fieldcapture.ala.org.au/';
 setting.description = 'The body of the email sent to a user when their elevated access will expire 1 month from now';
 db.setting.save(setting);
 
@@ -112,8 +112,8 @@ merit.accessManagementOptions = {
     "expireUsersAfterPeriodInactive": "P24M",
     "warnUsersAfterPeriodInactive": "P23M"
 };
-merit.emailFromAddress = "merit@awe.gov.au";
-merit.emailReplyToAddress = "merit@awe.gov.au";
+merit.emailFromAddress = "merit@ala.org.au";
+merit.emailReplyToAddress = "merit@ala.org.au";
 
 // Add the activity type facet
 const activityTypeField = 'activities.type.keyword';
