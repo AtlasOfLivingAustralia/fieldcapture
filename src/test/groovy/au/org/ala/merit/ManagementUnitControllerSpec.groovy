d61adc5d821cdcdcc96e38bd20fa55bec0e0788e
salomon-j <90952854+salomon-j@users.noreply.github.com>
2022-05-13 13:13:35 +1000

    commit pre-pop for the two tables #2525

--- src/test/groovy/au/org/ala/merit/ManagementUnitControllerSpec.groovy
+++ src/test/groovy/au/org/ala/merit/ManagementUnitControllerSpec.groovy
@@ -450,7 +450,21 @@ class ManagementUnitControllerSpec extends Specification implements ControllerUn
 
     def "the controller can pre-pop the value for some fields in RLP core services annual reporting"() {
         setup:
-        Map stubModel = [data:[serviceSubcontracted2020_21:'250000', investment2020_21:'570855']]
+        Map stubModel = [data:[serviceSubcontracted2020_21:250000,
+                               investment2020_21:570855,
+                               organisationWorkForceByFinancialYear:
+                                       [
+                                               [organisationWorkforce2020_21:1, organisationWorkforceType:"Indigenous FTE deployed within the Service Provider's organisation engaged in delivering the Services"],
+                                               [organisationWorkforce2020_21:5, organisationWorkforceType:"TOTAL FTE Australian-based-workforce within the Service Provider's organisation engaged in delivering the Services"]
+                                       ],
+                               subcontractedWorkForceByFinancialYear:
+                                       [
+                                               [contractWorkforce2020_21:2, contractWorkforceType:"Indigenous FTE deployed on subcontracts to deliver the Services"],
+                                               [contractWorkforce2020_21:6, contractWorkforceType:"TOTAL FTE deployed on subcontracts to deliver the Services"]
+                                       ]
+                              ]
+
+                        ]
 
         when:
         params.managementUnitId = 'mu01'
@@ -459,9 +473,16 @@ class ManagementUnitControllerSpec extends Specification implements ControllerUn
 
         then:
         1 * reportService.getPreviousReportModel(params) >> stubModel
+        println response.json
         response.json.managementUnitId == 'mu01'
-        response.json.model.data.serviceSubcontracted2020_21 == '250000'
-        response.json.model.data.investment2020_21 == '570855'
+        response.json.model.data.serviceSubcontracted2020_21 == 250000
+        response.json.model.data.investment2020_21 == 570855
+        response.json.model.data.organisationWorkForceByFinancialYear.size() == 2
+        response.json.model.data.organisationWorkForceByFinancialYear[0].organisationWorkforce2020_21 == 1
+        response.json.model.data.organisationWorkForceByFinancialYear[1].organisationWorkforce2020_21 == 5
+        response.json.model.data.subcontractedWorkForceByFinancialYear.size() == 2
+        response.json.model.data.subcontractedWorkForceByFinancialYear[0].contractWorkforce2020_21 == 2
+        response.json.model.data.subcontractedWorkForceByFinancialYear[1].contractWorkforce2020_21 == 6
 
     }
 
