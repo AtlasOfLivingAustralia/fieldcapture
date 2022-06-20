19c46981c1499b8869300fd89b4b2c1ad2837c99
salomon-j <90952854+salomon-j@users.noreply.github.com>
2022-06-20 16:51:02 +1000

    commit latest version Supporting Communities program/subprogram script #2565

--- src/main/scripts/releases/2.4/adhoc/createSupportingCommunitiesProgram.js
+++ src/main/scripts/releases/2.4/adhoc/createSupportingCommunitiesProgram.js
@@ -49,8 +49,18 @@ var programConfig = {
                 "canSubmitDuringReportingPeriod": true
             },
             {
+                "firstReportingPeriodEnd": "2024-06-30T14:00:00Z",
+                "reportType": "Administrative",
+                "reportDescriptionFormat": "Annual Progress Report %2$tY - %3$tY for %4$s",
+                "reportNameFormat": "Annual Progress Report %2$tY - %3$tY",
+                "reportingPeriodInMonths": 12,
+                "description": "",
+                "category": "Annual Progress Reporting",
+                "activityType": "RLP Annual Report"
+            },
+            {
                 "reportType": "Single",
-                "firstReportingPeriodEnd": "2022-06-30T14:00:00Z",
+                "firstReportingPeriodEnd": "2023-06-30T14:00:00Z",
                 "reportDescriptionFormat": "Outcomes Report 1 for %4$s",
                 "reportNameFormat": "Outcomes Report 1",
                 "reportingPeriodInMonths": 36,
@@ -151,25 +161,35 @@ var outcomes = [
     {
         "priorities": [
             {
-                "category": "Land Management"
+                "category": "Ramsar"
             }
         ],
         "targeted": true,
-        "shortDescription": "Soil Condition",
-        "type": "primary",
-        "category": "agriculture",
-        "outcome": "5. By 2023, there is an increase in the awareness and adoption of land management practices that improve and protect the condition of soil, biodiversity and vegetation."
+        "shortDescription": "Ramsar Sites",
+        "category": "environment",
+        "outcome": "1. By 2023, there is restoration of, and reduction in threats to, the ecological character of Ramsar sites, through the implementation of priority actions"
     },
     {
         "priorities": [
             {
-                "category": "Sustainable Agriculture"
+                "category": "Threatened Species"
             }
         ],
-        "shortDescription": "Climate / Weather Adaption",
-        "type": "secondary",
-        "category": "agriculture",
-        "outcome": "6. By 2023, there is an increase in the capacity of agriculture systems to adapt to significant changes in climate and market demands for information on provenance and sustainable production."
+        "targeted": true,
+        "shortDescription": "Threatened Species Strategy",
+        "category": "environment",
+        "outcome": "2. By 2023, the trajectory of species targeted under the Threatened Species Strategy, and other EPBC Act priority species, is stabilised or improved."
+    },
+    {
+        "priorities": [
+            {
+                "category": "Threatened Ecological Communities"
+            }
+        ],
+        "targeted": true,
+        "shortDescription": "Threatened Ecological Communities",
+        "category": "environment",
+        "outcome": "4. By 2023, the implementation of priority actions is leading to an improvement in the condition of EPBC Act listed Threatened Ecological Communities."
     }
 ]
 
