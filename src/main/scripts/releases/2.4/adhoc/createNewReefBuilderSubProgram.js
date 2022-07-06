624af65218592566c0d577363410367a146ae959
salomon-j <90952854+salomon-j@users.noreply.github.com>
2022-07-06 16:55:03 +1000

    Script to create new program/subprogram #2600

---
+++ src/main/scripts/releases/2.4/adhoc/createNewReefBuilderSubProgram.js
@@ -0,0 +1,57 @@
+load('../../../utils/uuid.js');
+
+let refProgram = "Reef Trust 7 - Islands Restoration";
+var subprograms = ["Reef Builder – the coastal communities recovery project"]
+
+subprograms.forEach(function (subProgram) {
+    var now = ISODate();
+    var newProgram = db.program.find({name: refProgram}).next();
+    delete newProgram._id
+    delete newProgram.programId
+    newProgram.name = subProgram
+    newProgram.programId = UUID.generate()
+    newProgram.dateCreated = now
+    newProgram.lastUpdated = now
+    newProgram.status = "active"
+    var program = db.program.find({name: subProgram})
+    if (!program.hasNext()) {
+        db.program.insert(newProgram);
+    } else {
+        print("Program Already Exist: " + subProgram)
+    }
+});
+
+var programConfig = {
+    outcomes: [],
+    priorities: [],
+    config: {
+        "excludes": [
+            "DATA_SETS"
+        ],
+        "requiresActivityLocking": true,
+        "projectTemplate": "index",
+        "meriPlanTemplate": "configurableMeriPlan",
+        "projectReports": [
+            {
+                "reportType": "Activity",
+                "weekDaysToCompleteReport": 30,
+                "reportDescriptionFormat": "Stage %1d for ${project.name}",
+                "reportNameFormat": "Stage %1d",
+                "reportingPeriodInMonths": 6,
+                "reportsAlignedToCalendar": true
+            }
+        ]
+    }
+};
+
+subprograms.forEach(function (subprogram){
+    var program = db.program.find({name: subprogram});
+    while(program.hasNext()){
+        var p = program.next();
+            p.outcomes = programConfig.outcomes;
+            p.config = programConfig.config;
+            p.priorities = programConfig.priorities;
+
+        db.program.save(p);
+    }
+});
