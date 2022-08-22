d3f15a004467e53646a056c8c108f6d6264eda0d
salomon-j <90952854+salomon-j@users.noreply.github.com>
2022-08-22 17:55:33 +1000

    commit update to create two different icon sizes #2660

--- grails-app/assets/javascripts/sites.js
+++ grails-app/assets/javascripts/sites.js
@@ -845,6 +845,7 @@ var AlaMapAdapter = function(map, options) {
         })
     };
 
+
     function highlightLayer(layer) {
         if (_.isFunction(layer.eachLayer)) { // Layers created from MultiPolygons & MultiPolyLines have nested layers
             layer.eachLayer(highlightLayer);
@@ -867,10 +868,7 @@ var AlaMapAdapter = function(map, options) {
                 }
             }
             else if (options.icon) {
-                var icon = options.icon;
-                icon.options.iconSize = [icon.options.iconSize[0] * 1.5, icon.options.iconSize[1] * 1.5];
-                icon.options.iconAnchor = [icon.options.iconAnchor[0] * 1.5, icon.options.iconAnchor[1] * 1.5];
-                layer.setIcon(icon);
+                layer.setIcon(createHighlightIcon(layer));
             }
         }
     }
@@ -894,10 +892,7 @@ var AlaMapAdapter = function(map, options) {
                 }
             }
             else if (layer.options && layer.options.icon) {
-                var icon = layer.options.icon;
-                icon.options.iconSize = [icon.options.iconSize[0] / 1.5, icon.options.iconSize[1] / 1.5];
-                icon.options.iconAnchor = [icon.options.iconAnchor[0] / 1.5, icon.options.iconAnchor[1] / 1.5];
-                layer.setIcon(icon);
+                layer.setIcon(createNormalIcon(layer));
             }
         }
     }
@@ -1314,4 +1309,19 @@ function loadAndConfigureSitePhotoPoints(targetElementSelector) {
         nextEffect: 'fade',
         previousEffect: 'fade'
     });
+}
+
+function createHighlightIcon(layer) {
+    var options = layer.options;
+    var icon = options.icon;
+    icon.options.iconSize = [38, 95];
+    icon.options.iconAnchor = [22, 94];
+    return options.icon
+}
+function createNormalIcon(layer) {
+    var options = layer.options;
+    var icon = options.icon;
+    icon.options.iconSize = [19, 46];
+    icon.options.iconAnchor = [11, 47];
+    return options.icon
 }
