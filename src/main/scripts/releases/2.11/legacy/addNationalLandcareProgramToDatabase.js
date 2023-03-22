794090b6484a004f7ff6296c62b3613c218a90b6
salomon-j <90952854+salomon-j@users.noreply.github.com>
2023-03-22 12:04:54 +1100

    commit update on excludes and activities #2741

--- src/main/scripts/releases/2.11/legacy/addNationalLandcareProgramToDatabase.js
+++ src/main/scripts/releases/2.11/legacy/addNationalLandcareProgramToDatabase.js
@@ -3,7 +3,90 @@ load('../../../utils/program.js');
 
 var nationalLandcare = createOrFindProgram('National Landcare Programme');
 
-var subprograms = ["20 Million Trees Cumberland Conservation Corridor Land Management","20 Million Trees Grants", "20 Million Trees Grants Round 2","20 Million Trees Grants Round 3","20 Million Trees Service Providers Tranche 2","20 Million Trees Service Providers Tranche 3","25th Anniversary Landcare Grants 2014-15","Landcare Network Grants 2014-16","Local Programmes","Regional Delivery","Regional Funding"];
+var subprograms = ["Regional Delivery","Local Programmes", "25th Anniversary Landcare Grants 2014-15","20 Million Trees Grants Round 1","World Heritage","Regional Funding","Landcare Network Grants 2014-16", "20 Million Trees Grants Round 2", "20 Million Trees West Melbourne", "20 Million Trees Discretionary Grants","20 Million Trees Cumberland Conservation Corridor Grants","20 Million Trees Cumberland Conservation Corridor Land Management","20 Million Trees Service Providers Tranche 1","20 Million Trees Service Providers","20 Million Trees Service Providers Tranche 2","20 Million Trees Grants Round 3","20 Million Trees Service Providers Tranche 3"];
+var excludes25thAnniversaryLandcareGrants201415 = ["DATA_SETS","RISKS_AND_THREATS"];
+
+var activities25thAnniversaryLandcareGrants201415 = [
+    {name:"Indigenous Employment and Businesses"},
+    {name:"Outcomes, Evaluation and Learning - final report"},
+    {name:"Project Administration"},
+    {name:"Progress, Outcomes and Learning - stage report"},
+    {name:"Fauna Survey - general"},
+    {name:"Flora Survey - general"},
+    {name:"Pest Animal Survey"},
+    {name:"Plant Survival Survey"},
+    {name:"Site Monitoring Plan"},
+    {name:"Water Quality Survey"},
+    {name:"Weed Mapping & Monitoring"},
+    {name:"Vegetation Assessment - Commonwealth government methodology"},
+    {name:"Community Participation and Engagement"},
+    {name:"Debris Removal"},
+    {name:"Disease Management"},
+    {name:"Erosion Management"},
+    {name:"Fencing"},
+    {name:"Conservation Grazing Management"},
+    {name:"Fire Management"},
+    {name:"Heritage Conservation"},
+    {name:"Management Plan Development"},
+    {name:"Management Practice Change"},
+    {name:"Conservation Actions for Species and Communities"},
+    {name:"Pest Management"},
+    {name:"Plant Propagation"},
+    {name:"Public Access and Infrastructure"},
+    {name:"Research"},
+    {name:"Revegetation"},
+    {name:"Seed Collection"},
+    {name:"Site Preparation"},
+    {name:"Water Management"},
+    {name:"Weed Treatment"},
+    {name:"Works Planning and Risk"},
+    {name:"25th Anniversary Landcare Grants - Progress Report"},
+    {name:"25th Anniversary Landcare Grants - Final Report"},
+    {name:"Indigenous Knowledge Transfer"},
+    {name:"Training and Skills Development"}
+]
+
+var activitiesRegionalFunding = [
+    {name:"Regional Funding Final Report"},
+    {name:"Indigenous Employment and Businesses"},
+    {name:"Outcomes, Evaluation and Learning - final report"},
+    {name:"Project Administration"},
+    {name:"Progress, Outcomes and Learning - stage report"},
+    {name:"Stage Report"},
+    {name:"Fauna Survey - general"},
+    {name:"Flora Survey - general"},
+    {name:"Pest Animal Survey"},
+    {name:"Plant Survival Survey"},
+    {name:"Site Monitoring Plan"},
+    {name:"Water Quality Survey"},
+    {name:"Weed Mapping & Monitoring"},
+    {name:"Vegetation Assessment - Commonwealth government methodology"},
+    {name:"Community Grants"},
+    {name:"Community Participation and Engagement"},
+    {name:"Debris Removal"},
+    {name:"Disease Management"},
+    {name:"Erosion Management"},
+    {name:"Fencing"},
+    {name:"Conservation Grazing Management"},
+    {name:"Fire Management"},
+    {name:"Heritage Conservation"},
+    {name:"Management Plan Development"},
+    {name:"Management Practice Change"},
+    {name:"Conservation Actions for Species and Communities"},
+    {name:"Pest Management"},
+    {name:"Plant Propagation"},
+    {name:"Public Access and Infrastructure"},
+    {name:"Research"},
+    {name:"Revegetation"},
+    {name:"Seed Collection"},
+    {name:"Site Preparation"},
+    {name:"Water Management"},
+    {name:"Weed Treatment"},
+    {name:"Works Planning and Risk"},
+    {name:"Indigenous Knowledge Transfer"},
+    {name:"Training and Skills Development"}
+]
+
 var config = {
     "projectReports": [
         {
@@ -15,8 +98,8 @@ var config = {
         }
     ],
     excludes:["DATA_SETS"],
-    "projectTemplate": "default",
-    "activities": [
+    projectTemplate: "default",
+    activities: [
         {name:"Indigenous Employment and Businesses"},
         {name:"Outcomes, Evaluation and Learning - final report"},
         {name:"Project Administration"},
@@ -50,6 +133,7 @@ var config = {
         {name:"Water Management"},
         {name:"Weed Treatment"},
         {name:"Works Planning and Risk"},
+        {name:"Post revegetation site management"},
         {name:"Indigenous Knowledge Transfer"},
         {name:"Training and Skills Development"}
     ],
@@ -64,6 +148,16 @@ db.project.updateMany({associatedProgram:nationalLandcare.name,associatedSubProg
 for (var i=0; i<subprograms.length; i++) {
     var sub = createOrFindProgram(subprograms[i], nationalLandcare._id);
     sub.config = config;
+
+    if (sub.name === "25th Anniversary Landcare Grants 2014-15"){
+        config.excludes = excludes25thAnniversaryLandcareGrants201415;
+    }
+    if (sub.name === "25th Anniversary Landcare Grants 2014-15"){
+        config.activities = activities25thAnniversaryLandcareGrants201415;
+    }
+    if (sub.name === "Regional Funding"){
+        config.activities = activitiesRegionalFunding;
+    }
     print("******************************* "+sub.programId+" ****************************");
     // db.program.save(sub);
     //inserts the config
