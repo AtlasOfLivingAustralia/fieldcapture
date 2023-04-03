2599fcbd16c4c8c95c7f68627a6e075cc46d38f3
Chris <chris.godwin.ala@gmail.com>
2023-04-03 15:22:53 +1000

    Added a script to run all program creation scripts #2741

---
+++ src/main/scripts/releases/2.11/legacy/runAll.sh
@@ -0,0 +1,7 @@
+mongo -u ecodata -p "$1" ecodata addCumberlandPlainProgramToDatabase.js
+mongo -u ecodata -p "$1" ecodata addNationalLandcareProgramToDatabase.js
+mongo -u ecodata -p "$1" ecodata addGreenArmyProgramToDatabase.js
+mongo -u ecodata -p "$1" ecodata addReefTrustProgramToDatabase.js
+mongo -u ecodata -p "$1" ecodata addCaringForOurCountry2ProgramToDatabase.js
+mongo -u ecodata -p "$1" ecodata addReef2050PlanProgramToDatabase.js
+mongo -u ecodata -p "$1" ecodata addBiodiversityFundProgramToDatabase.js
