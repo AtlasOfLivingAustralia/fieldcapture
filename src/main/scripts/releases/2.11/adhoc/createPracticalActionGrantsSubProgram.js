f6884994e26a9e865ee6bb5355c5564d5f6cd3a3
salomon-j <90952854+salomon-j@users.noreply.github.com>
2023-04-27 11:57:17 +1000

    commit program config updates #2825

--- src/main/scripts/releases/2.11/adhoc/createPracticalActionGrantsSubProgram.js
+++ src/main/scripts/releases/2.11/adhoc/createPracticalActionGrantsSubProgram.js
@@ -33,6 +33,24 @@ var projectConfig = {
     config: {
         "meriPlanContents": [
             {
+                "template": "name",
+                "model": {
+                    "helpTextHeading": "A succinct overview of the project: (i) what will be done and (ii) why it will be done",
+                    "maxSize": "1000",
+                    "placeholder": "[Free text; limit response to 1000 characters (approx. 150 words)]",
+                    "explanation": ""
+                }
+            },
+            {
+                "template": "description",
+                "model": {
+                    "helpTextHeading": "A succinct overview of the project: (i) what will be done and (ii) why it will be done",
+                    "maxSize": "1000",
+                    "placeholder": "[Free text; limit response to 1000 characters (approx. 150 words)]",
+                    "explanation": " Please provide a short succinct description of this project. The description should state what will be done and why it will be done. This project description will be publicly visible on the project overview page in MERIT"
+                }
+            },
+            {
                 "template": "assets",
                 "model": {
                     "priorityCategories": [
@@ -72,30 +90,7 @@ var projectConfig = {
                 "model": {
                     "subtitle": "Please provide outcome statements. Outcomes statements should: <br/>- Clearly describe the intent of the project, specifically the benefit or change that the project is expected to deliver by June 2022;<br/>- Be expressed as a SMART statement (Specific, Measurable, Attainable, Realistic and Time-bound); and<br/>- Ensure that the outcomes are measurable with consideration to the monitoring methodology provided below",
                     "placeholder": "By 30 June 2021, [Free text]",
-                    "title": "Outcome statements"
-                }
-            },
-            {
-                "template": "sectionHeading",
-                "model": {
-                    "heading": "Project Details"
-                }
-            },
-            {
-                "template": "description",
-                "model": {
-                    "helpTextHeading": "A succinct overview of the project: (i) what will be done and (ii) why it will be done",
-                    "maxSize": "1000",
-                    "placeholder": "[Free text; limit response to 1000 characters (approx. 150 words)]",
-                    "explanation": " Please provide a short succinct description of this project. The description should state what will be done and why it will be done. This project description will be publicly visible on the project overview page in MERIT"
-                }
-            },
-            {
-                "template": "projectPartnerships",
-                "model": {
-                    "namePlaceHolder": "[Free text]",
-                    "helpTextPartnerNature": "If partnership with an organisation: provide the name of the organisation and the role they will play/how you will support them. If partnering with community groups or members of the public: indicate each group or individual you will engage with",
-                    "partnershipPlaceHolder": "[Free text]"
+                    "title": "Project Outcome"
                 }
             },
             {
@@ -130,6 +125,13 @@ var projectConfig = {
                 }
             },
             {
+                "template": "adaptiveManagement",
+                "model": {
+                    "title": "Project Review, Evaluation and Improvement Methodology and Approach",
+                    "explanation": "Outline the methods and processes that will enable adaptive management during the lifetime of this project"
+                }
+            },
+            {
                 "template": "nationalAndRegionalPlans",
                 "model": {
                     "documentNameHelpText": "List the name of the Recovery Plan or Conservation Advice for species listed under the Environment Protection and Biodiversity Conservation Act 1999 (if applicable), or state recovery plan.",
@@ -140,8 +142,16 @@ var projectConfig = {
                 "template": "serviceTargets",
                 "model": {
                     "showTargetDate": true,
-                    "title": "Actions and Targets Table",
-                    "serviceName": "Service"
+                    "title": "Activities and Targets Table",
+                    "serviceName": "Activities"
+                }
+            },
+            {
+                "template": "projectPartnerships",
+                "model": {
+                    "namePlaceHolder": "[Free text]",
+                    "helpTextPartnerNature": "If partnership with an organisation: provide the name of the organisation and the role they will play/how you will support them. If partnering with community groups or members of the public: indicate each group or individual you will engage with",
+                    "partnershipPlaceHolder": "[Free text]"
                 }
             },
             {
@@ -157,6 +167,256 @@ var projectConfig = {
             }
         ],
         "excludes": [],
+        "programServiceConfig": {
+            "serviceFormName": "Priority Threatened Species Progress Report",
+            "programServices": [
+                {
+                    "serviceTargets": [
+                        "0df7c177-2864-4a25-b420-2cf3c45ce749"
+                    ],
+                    "serviceId": 1
+                },
+                {
+                    "serviceTargets": [
+                        "69deaaf9-cdc2-439a-b684-4cffdc7f224e"
+                    ],
+                    "serviceId": 2
+                },
+                {
+                    "serviceTargets": [
+                        "1f8161bc-908b-4ec8-ab7f-edac973a657d",
+                        "88908921-1248-4a7c-b185-51c625c737e3",
+                        "26a8213e-1770-4dc4-8f99-7e6302197504",
+                        "15a49c6f-2177-4183-9955-c7e487970171",
+                        "fe9f1a6c-e614-489a-80fa-4d6d76f1cf95",
+                        "f7089b5b-333f-4f33-b0df-f7ef88e9f683"
+                    ],
+                    "serviceId": 3
+                },
+                {
+                    "serviceTargets": [
+                        "aa0c6b29-285e-4344-987e-dfeaf1d95648",
+                        "c464b652-be5e-4658-b62f-02bf1a80bcf8",
+                        "a901232d-8244-40dd-9bb7-ff9e4e9dbeac"
+                    ],
+                    "serviceId": 4
+                },
+                {
+                    "serviceTargets": [
+                        "55d76c03-c89d-40fe-867b-93f7a48ff9c1",
+                        "3cbf653f-f74c-4066-81d2-e3f78268185c",
+                        "3855d565-3b77-497b-90af-addb271aa598",
+                        "5dbfb32a-5933-4d8a-9937-41f350fb5f75"
+                    ],
+                    "serviceId": 5
+                },
+                {
+                    "serviceTargets": [
+                        "d97037d2-7ee4-429b-bd26-cdcc5a269d9d",
+                        "e48faf01-72eb-479c-be9b-d2d71d254fa4"
+                    ],
+                    "serviceId": 6
+                },
+                {
+                    "serviceTargets": [
+                        "482bdf4e-6f7a-4bdf-80d5-d619ac7cdf50",
+                        "2ef32f94-14f1-4878-9eb7-c115fa18ce25"
+                    ],
+                    "serviceId": 7
+                },
+                {
+                    "serviceTargets": [
+                        "7709de92-0f85-490b-bc5f-2748b3db42c2",
+                        "8025b157-44d7-4283-bc1c-f40fb9b99501",
+                        "0ec5fd50-13e2-4bde-8e01-5955e7145cae"
+                    ],
+                    "serviceId": 8
+                },
+                {
+                    "serviceTargets": [
+                        "46925c6c-e222-4f6a-8553-c69929929d10",
+                        "757d6c9e-ec24-486f-a128-acc9bfb87830",
+                        "ffa44bad-209d-414a-bc96-e8ca3c96092f"
+                    ],
+                    "serviceId": 9
+                },
+                {
+                    "serviceTargets": [
+                        "edb6e94a-d781-447c-8792-5e06e5c912a8",
+                        "a3afea6e-711c-4ef2-bb20-6d2630b7ee93"
+                    ],
+                    "serviceId": 10
+                },
+                {
+                    "serviceTargets": [
+                        "607e2cd4-d7c5-4d5b-867f-c2c8e7b62948",
+                        "b7c067e3-6ae7-4e76-809a-312165b75f94"
+                    ],
+                    "serviceId": 11
+                },
+                {
+                    "serviceTargets": [
+                        "d1c10295-05e5-4265-a5f1-8a5683af2efe"
+                    ],
+                    "serviceId": 12
+                },
+                {
+                    "serviceTargets": [
+                        "011a161f-7275-4b5e-986e-3fe4640d0265",
+                        "3e402423-3e0b-4549-9e09-2b71329ea069"
+                    ],
+                    "serviceId": 13
+                },
+                {
+                    "serviceTargets": [
+                        "c2dc6f91-ccb1-412e-99d0-a842a4ac4b03"
+                    ],
+                    "serviceId": 14
+                },
+                {
+                    "serviceTargets": [
+                        "def4e2af-dcad-4a15-8336-3765e6671f08",
+                        "cf3ccbf5-d031-4a22-a746-961cdefa7318"
+                    ],
+                    "serviceId": 15
+                },
+                {
+                    "serviceTargets": [
+                        "c46842b6-d7b6-4917-b56f-f1b0594663fa",
+                        "ab822db5-ed79-47af-badc-5a3772dab87d"
+                    ],
+                    "serviceId": 16
+                },
+                {
+                    "serviceTargets": [
+                        "2d877a91-6312-4c44-9ae1-2494ea3e43db"
+                    ],
+                    "serviceId": 17
+                },
+                {
+                    "serviceTargets": [
+                        "d4ba13a1-00c8-4e7f-8463-36b6ea37eee6",
+                        "01ee5719-e814-43ce-a1b1-4e26063b5a6c"
+                    ],
+                    "serviceId": 18
+                },
+                {
+                    "serviceTargets": [
+                        "4bcab901-879a-402d-83f3-01528c6c86a5"
+                    ],
+                    "serviceId": 19
+                },
+                {
+                    "serviceTargets": [
+                        "45994b98-21f1-4927-a03e-3d940ac75116"
+                    ],
+                    "serviceId": 20
+                },
+                {
+                    "serviceTargets": [
+                        "6eaa061c-b77b-4440-8e8f-7ebaa2ff6207"
+                    ],
+                    "serviceId": 21
+                },
+                {
+                    "serviceTargets": [
+                        "0e887410-a3c5-49ca-a6f5-0f2f6fae30db"
+                    ],
+                    "serviceId": 22
+                },
+                {
+                    "serviceTargets": [
+                        "725d9365-0889-4355-8a7f-a21ef260c468",
+                        "996e3dc7-1376-47ad-a941-648cbae246b4"
+                    ],
+                    "serviceId": 23
+                },
+                {
+                    "serviceTargets": [
+                        "0f11a699-6063-4e91-96ca-53e45cf26b80",
+                        "1144fc3d-4d31-42bd-b9fb-ab5f91db37ca"
+                    ],
+                    "serviceId": 24
+                },
+                {
+                    "serviceTargets": [
+                        "8a5dac4a-0bef-431c-b857-2085eca9ae7c",
+                        "3c2c4aaa-fd5f-43d8-a72f-3567e6dea6f4"
+                    ],
+                    "serviceId": 25
+                },
+                {
+                    "serviceTargets": [
+                        "ed30b80b-7bb9-4c04-9949-093df64d124c",
+                        "3d4f1932-7a5b-45d6-ae68-d0571860ea94"
+                    ],
+                    "serviceId": 26
+                },
+                {
+                    "serviceTargets": [
+                        "a516c78d-740f-463b-a1ce-5b02b8c82dd3",
+                        "4cbcb2b5-45cd-42dc-96bf-a9a181a4865b",
+                        "fbc45154-1d60-4f5e-a484-fdff514f9d51",
+                        "85191c99-f56d-46e6-9311-a58c1f37965d"
+                    ],
+                    "serviceId": 27
+                },
+                {
+                    "serviceTargets": [
+                        "6ab3298e-c24b-49d8-9f86-441e71858b6a",
+                        "f23f9fb0-99ec-4fbf-ade3-b6581fe25dcf",
+                        "fd77b1b1-8234-4d06-8a02-aea6f4abb01d",
+                        "f38fbd9e-d208-4750-96ce-3c032ad37684"
+                    ],
+                    "serviceId": 28
+                },
+                {
+                    "serviceTargets": [
+                        "4f747371-fa5f-4200-ae37-6cd59d268fe8"
+                    ],
+                    "serviceId": 29
+                },
+                {
+                    "serviceTargets": [
+                        "685d61e9-2ebd-4198-a83a-ac7a2fc1477a"
+                    ],
+                    "serviceId": 30
+                },
+                {
+                    "serviceTargets": [
+                        "91387f2b-258d-4325-aa60-828d1acf6ac6"
+                    ],
+                    "serviceId": 31
+                },
+                {
+                    "serviceTargets": [
+                        "ba3d0a20-1e4d-404a-9907-b95239499c2f",
+                        "aa92f559-6260-4947-a714-ae6b80776b47"
+                    ],
+                    "serviceId": 32
+                },
+                {
+                    "serviceTargets": [
+                        "28dd9736-b66a-4ab4-9111-504d5cffba88",
+                        "4670a4ba-62bb-401c-a7ed-4fbd8dc5999c"
+                    ],
+                    "serviceId": 33
+                },
+                {
+                    "serviceTargets": [
+                        "0f9ef068-b2f9-4e6f-9ab5-521857b036f4"
+                    ],
+                    "serviceId": 34
+                },
+                {
+                    "serviceTargets": [
+                        "dea1ff8b-f4eb-4987-8073-500bbbf97fcd",
+                        "d48e05f4-a1cb-40e6-a4a3-bafa86b137f0"
+                    ],
+                    "serviceId": 35
+                }
+            ]
+        },
         "visibility": "public",
         "organisationRelationship": "Service Provider",
         "excludeFinancialYearData": true,
@@ -183,19 +443,9 @@ var projectConfig = {
                 "minimumReportDurationInDays": 3,
                 "label": "Semester",
                 "category": "Progress Reports",
-                "activityType": "Koala Conservation Progress Report",
+                "activityType": "Priority Threatened Species Progress Report",
                 "reportsAlignedToCalendar": true,
                 "canSubmitDuringReportingPeriod": true
-            },
-            {
-                "firstReportingPeriodEnd": "2024-06-30T14:00:00Z",
-                "reportType": "Administrative",
-                "reportDescriptionFormat": "Annual Progress Report %2$tY - %3$tY for %4$s",
-                "reportNameFormat": "Annual Progress Report %2$tY - %3$tY",
-                "reportingPeriodInMonths": 12,
-                "description": "",
-                "category": "Annual Progress Reporting",
-                "activityType": "RLP Annual Report"
             },
             {
                 "reportType": "Single",
@@ -208,7 +458,7 @@ var projectConfig = {
                 "label": "Final Report",
                 "category": "Final Report",
                 "reportsAlignedToCalendar": false,
-                "activityType": "Final Report",
+                "activityType": "Koala Conservation Final Report",
                 "alignToOwnerStart": true
             }
         ],
