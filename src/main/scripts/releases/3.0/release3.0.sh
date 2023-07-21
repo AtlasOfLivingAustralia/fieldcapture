dbd41e0753c5ff234e6f16b408517834bb20bd51
Chris <chris.godwin.ala@gmail.com>
2023-07-21 15:44:55 +1000

    Script to do database update for 3.0 #2950

--- src/main/scripts/releases/3.0/release3.0.sh
+++ src/main/scripts/releases/3.0/release3.0.sh
@@ -7,6 +7,6 @@ mongosh -u ecodata -p "$1" ecodata addCaringForOurCountry2ProgramToDatabase.js
 mongosh -u ecodata -p "$1" ecodata addReef2050PlanProgramToDatabase.js
 mongosh -u ecodata -p "$1" ecodata addBiodiversityFundProgramToDatabase.js
 
-cd ../3.0
+cd ../../3.0
 mongo -u ecodata -p "$1" ecodata configureNewProgram.js
 mongo -u ecodata -p "$1" ecodata configureOrganisationReports.js
