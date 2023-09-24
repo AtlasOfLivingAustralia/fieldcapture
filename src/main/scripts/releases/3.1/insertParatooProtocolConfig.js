69fd04e7030b8f204d51d6baf663f62767278429
Chris <chris.godwin.ala@gmail.com>
2023-09-25 09:08:02 +1000

    Protocol mapping script WIP #2971

--- src/main/scripts/releases/3.1/insertParatooProtocolConfig.js
+++ src/main/scripts/releases/3.1/insertParatooProtocolConfig.js
@@ -1,4 +1,5 @@
 var protocolConfig = {
+    // Vegetation Mapping Survey
     'cd2cbbc7-2f17-4b0f-91b4-06f46e9c90f2': {
         apiEndpoint:'vegetation-mapping-observations',
         usesPlotLayout:false,
@@ -28,6 +29,24 @@ var protocolConfig = {
         startDatePath: 'attributes.start_date_time',
         endDatePath: 'attributes.end_date_time',
         surveyIdPath: 'attributes.surveyId'
+    },
+    // Pest Fauna Control Activities
+    '80360ceb-bd6d-4ed4-b2ea-9bd45d101d0e': {
+        apiEndpoint:'pest-fauna-control-activities',
+        usesPlotLayout:false,
+        startDatePath: 'attributes.start_time',
+        endDatePath: 'attributes.end_time',
+        geometryType: "LineString",
+        point1: "attributes.start_location", // Type string: format: Lat: -35.2592426, Lng: 149.0651714
+        point2: "attributes.end_location" // Format: Lat: -35.2592426, Lng: 149.0651714
+
+    },
+    // Basal Area DBH
+    '5005b0af-4360-4a8c-a203-b2c9e440547e': {
+        apiEndpoint:'basal-area-dbh-measure-surveys',
+        usesPlotLayout:true,
+        startDatePath: 'attributes.start_date',
+        endDatePath: 'attributes.start_date',
     }
 };
 
@@ -38,4 +57,9 @@ var setting = {
     dateCreated: ISODate(),
     lastUpdated: ISODate()
 };
-db.setting.insertOne(setting);
+if (db.setting.findOne({key: key})) {
+    db.setting.replaceOne({key: key}, setting);
+}
+else {
+    db.setting.insertOne(setting);
+}
