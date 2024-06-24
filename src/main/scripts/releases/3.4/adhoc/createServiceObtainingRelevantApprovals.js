d7cc51c7ba54879f0a6ecdf3006061b090a68142
temi <temi.varghese@csiro.au>
2024-06-25 06:10:50 +1000

    #3228 - added two scores and one service

--- src/main/scripts/releases/3.4/adhoc/createServiceObtainingRelevantApprovals.js
+++ src/main/scripts/releases/3.4/adhoc/createServiceObtainingRelevantApprovals.js
@@ -1,4 +1,5 @@
-load("../../utils/addService.js")
+load("../../../utils/addService.js")
+load("../../../utils/program.js")
 var userId = "4228"
 var serviceName = "Developing farm/project/site management plan"
 var sectionName = "Management plan development"
@@ -7,19 +8,24 @@ var newOutputs = [
     {formName: "Grants and Others Progress Report", sectionName: sectionName},
     {formName: "Procurement Output Report", sectionName: sectionName}
 ];
+// RDP panel procurements are excluded
+var excludedPrograms = ["Priority Places - Procurements", "Priority Species - Procurements - RDP"]
+
+// find "Developing farm/project/site management plan" service
 var service = db.service.findOne({name: serviceName})
 if (!service) {
     throw "Service not found: " + serviceName
 }
 
-// add to all NHT programs
+// update service name to "Developing Management Plans" for all NHT programs
 var programLabels = service.programLabels || {}
-var nhtPrograms = db.program.find({'config.meriPlanContents.template':'extendedKeyThreats'}, {programId: true})
+var nhtPrograms = db.program.find({'config.meriPlanContents.template':'extendedKeyThreats', name: {"$nin": excludedPrograms} }, {programId: true})
 while(nhtPrograms.hasNext()) {
     var program = nhtPrograms.next()
     programLabels[program.programId] = {label: label}
 }
 
+// add outputs to service
 var outputs = service.outputs || [];
 for (var i = 0; i < newOutputs.length; i++) {
     if (!outputs.find(function (output) { return output.formName === newOutputs[i].formName && output.sectionName === newOutputs[i].sectionName })) {
@@ -31,6 +37,7 @@ db.service.updateOne({serviceId: service.serviceId}, {$set: {programLabels: prog
 service = db.service.findOne({name: serviceName});
 audit(service, service.serviceId, 'au.org.ala.ecodata.Service', userId, undefined, "Update");
 
+// add new scores associated with "Developing Management Plans" service
 var scores = [
     {
         "configuration": {
@@ -119,4 +126,31 @@ outputs = [
         sectionName: "Obtaining approvals"
     }
 ]
-addService("Obtaining Relevant Approvals", 46, undefined, undefined, outputs, userId)
+addService("Obtaining Relevant Approvals", NumberInt(46), undefined, undefined, outputs, userId)
+// Update program config with service and service targets
+var score = db.score.findOne({label: "Number of relevant approvals obtained"});
+if(!score)
+    throw "Score not found: Number of relevant approvals obtained";
+
+var programServices = [
+    {
+        serviceTargets: ["8113ab8a-17e6-43c9-be32-9ca72dd01454", "9f6f06ac-b055-48c1-9d80-b305acdeae79"],
+        serviceId: service.legacyId
+    },
+    {
+        serviceTargets: [score.scoreId],
+        serviceId: NumberInt(46),
+    }
+];
+
+var programs = db.program.find({name: {$nin: excludedPrograms}, 'config.meriPlanContents.template':'extendedKeyThreats'})
+while (programs.hasNext()) {
+    var program = programs.next();
+    programServices.forEach(function (programService) {
+        updateProgramServiceConfig(program, programService.serviceId, programService.serviceTargets);
+    });
+
+    db.program.updateOne({programId: program.programId}, {$set: {config: program.config}});
+    audit(program, program.programId, 'au.org.ala.ecodata.Program', userId, undefined, "Update");
+    print("Updated "+ program.name);
+}
--- src/main/scripts/utils/addService.js
+++ src/main/scripts/utils/addService.js
@@ -1,7 +1,12 @@
+var currentPath = pwd(),
+    devPath = true,
+    srcPath = devPath ? currentPath + "src/main/scripts/utils/" : currentPath
+
 load("../../../utils/uuid.js");
-load("../../../utils/audit.js");
+load( "../../../utils/audit.js");
 function addService (newServiceName, legacyId,  serviceFormName, sectionName, outputs, userId) {
     var eventType;
+    legacyId = NumberInt(legacyId);
     if (!outputs) {
         outputs = [
             {
