a4b6c9381ff682a647e43b3a71ae3b3786a3f029
chrisala <chris.godwin.ala@gmail.com>
2024-07-29 10:07:28 +1000

    Restored polling interval #3264

--- grails-app/assets/javascripts/base-bs4.js
+++ grails-app/assets/javascripts/base-bs4.js
@@ -14,7 +14,7 @@ function setupTimeoutWarning(options) {
     var $loginButton = $(options.loginButtonSelector);
 
     // Set up a timer that will periodically poll the server to keep the session alive
-    var intervalSeconds = 5 //* 60;
+    var intervalSeconds = 5 * 60;
 
     function fireKeepAlive() {
         $.ajax(options.keepSessionAliveUrl).fail(function (xhr) {
