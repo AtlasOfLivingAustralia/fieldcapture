7d2a02611be35323b7fb990dfeb1b2d6cc1997c7
chrisala <chris.godwin.ala@gmail.com>
2024-08-13 15:23:39 +1000

    Bumped facet limit #2691

--- grails-app/services/au/org/ala/merit/SearchService.groovy
+++ grails-app/services/au/org/ala/merit/SearchService.groovy
@@ -22,7 +22,7 @@ class SearchService {
     def elasticBaseUrl
     def userService
 
-    private static final int FACET_LIMIT = 1500
+    private static final int FACET_LIMIT = 3000
 
     @PostConstruct
     private void init() {
