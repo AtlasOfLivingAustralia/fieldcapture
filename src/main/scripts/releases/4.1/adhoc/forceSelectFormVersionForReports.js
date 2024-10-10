1be5d7f9ff67b5abf9d11b069e1f6c1221a5e6cf
temi <temi.varghese@csiro.au>
2024-10-10 14:41:29 +1100

    #3343 - CR commit

--- src/main/scripts/releases/4.1/adhoc/forceSelectFormVersionForReports.js
+++ src/main/scripts/releases/4.1/adhoc/forceSelectFormVersionForReports.js
@@ -1,8 +1,10 @@
+load('../../utils/audit.js');
 var programId = "834f6ec8-100b-44c2-9527-72d12d6edd89", projects = [], reports = [], activities = [],
-    reportName= "Progress Report 1", activities = [], newFormVersion = NumberInt(1), counter = 0;
+    reportName= "Progress Report 1", activities = [], newFormVersion = NumberInt(1), counter = 0,
+    user="system";
 
-db.project.find({programId: programId}).forEach(function(program) {
-    projects.push(program.projectId);
+db.project.find({programId: programId}).forEach(function(project) {
+    projects.push(project.projectId);
 });
 print("Projects: " + projects.length);
 db.report.find({projectId: {$in: projects}, name: reportName }).forEach(function(report) {
@@ -13,7 +15,9 @@ db.report.find({projectId: {$in: projects}, name: reportName }).forEach(function
 print("Reports length " + reports.length + " reports list " + reports);
 db.activity.find({activityId: {$in: activities}, progress: "planned"}).forEach(function(activity) {
     if (!activity.formVersion) {
+        activity.formVersion = newFormVersion;
         db.activity.updateOne({activityId: activity.activityId}, {$set: {formVersion: newFormVersion}});
+        audit(activity, activity.activityId, 'au.org.ala.ecodata.Activity', user);
         print("Activity " + activity.activityId + " updated.");
         counter++;
     }
