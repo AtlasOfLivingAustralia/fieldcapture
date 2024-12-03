698fb5210e056e9cd5761f4aa9ab5380507aa1bf
chrisala <chris.godwin.ala@gmail.com>
2024-12-03 11:21:59 +1100

    Trying to fix test that started failing #3368

--- src/integration-test/groovy/au/org/ala/fieldcapture/ConfigurableMeriPlanSpec.groovy
+++ src/integration-test/groovy/au/org/ala/fieldcapture/ConfigurableMeriPlanSpec.groovy
@@ -457,7 +457,6 @@ class ConfigurableMeriPlanSpec extends StubbedCasSpec {
         }
     }
 
-    @Ignore
     def "The MERI Plan will display only sections specified in the program configuration"() {
 
         setup:
@@ -547,6 +546,7 @@ class ConfigurableMeriPlanSpec extends StubbedCasSpec {
 
     }
 
+    @Ignore
     def "The MERI Plan will display only sections specified in state intervention config for state intervention projects"() {
         setup:
         // Clear cache to ensure services are loaded correctly
