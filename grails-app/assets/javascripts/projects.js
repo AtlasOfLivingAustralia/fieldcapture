d05f7227208633d960896adcd08f75b7ac68d04d
temi <temi.varghese@csiro.au>
2025-02-20 14:35:52 +1100

    #3432, AtlasOfLivingAutralia/ecodata#1047, AtlasOfLivingAutralia/ecodata#1049 - electorate is excluded state is added to exclude list - selecting primary electorate override automatically enables primary state override

--- grails-app/assets/javascripts/projects.js
+++ grails-app/assets/javascripts/projects.js
@@ -212,6 +212,13 @@ function ProjectViewModel(project) {
         }
     });
 
+    // if overriding primary electorate, then we must override primary state by default.
+    self.geographicInfo.overridePrimaryElectorate.subscribe(function(newValue) {
+        if (newValue) {
+            self.geographicInfo.overridePrimaryState(true);
+        }
+    })
+
     self.geographicInfo.primaryElectorate.subscribe(function(newValue) {
         if (newValue) {
             var electorate = findElectorate(newValue);
@@ -240,6 +247,22 @@ function ProjectViewModel(project) {
             });
         }
     });
+
+    /**
+     * Automatically add states of excluded electorates to excluded states field provided that state is
+     * available for selection.
+     */
+    self.geographicInfo.otherExcludedElectorates.subscribe(function(newOtherExcludedElectorates) {
+        var statesOfNewOtherExcludedElectorates = findStatesElectoratesBelong(newOtherExcludedElectorates),
+            availableStates = self.transients.states.statesToExclude();
+
+        statesOfNewOtherExcludedElectorates && statesOfNewOtherExcludedElectorates.forEach(function(state) {
+            if (availableStates.indexOf(state) > -1) {
+                self.geographicInfo.otherExcludedStates.push(state);
+            }
+        })
+    })
+
     self.transients.programs = [];
     self.transients.subprograms = {};
     self.transients.subprogramsToDisplay = ko.computed(function () {
@@ -254,6 +277,20 @@ function ProjectViewModel(project) {
             return electorate.name === electorateName;
         });
     };
+
+    function findStatesElectoratesBelong (electorates) {
+        var states = []
+        electorates && electorates.forEach(function(electorate) {
+            var electorateObj = findElectorate(electorate);
+            electorateObj && electorateObj.state && electorateObj.state.forEach (function (state) {
+                if (states.indexOf(state) === -1) {
+                    states.push.apply(states, electorateObj.state)
+                }
+            });
+        });
+
+        return states;
+    }
 
     var isBeforeToday = function(date) {
         return moment(date) < moment().startOf('day');
