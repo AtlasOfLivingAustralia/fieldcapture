32ae82adfd3111823176c639e86d074d3a827676
temi <temi.varghese@csiro.au>
2025-07-16 21:48:24 +1000

    #3565 - detach outputs of another project

--- src/main/scripts/releases/4.6/adhoc/tempDetachMonitorOutputs.js
+++ src/main/scripts/releases/4.6/adhoc/tempDetachMonitorOutputs.js
@@ -1,10 +1,15 @@
-const projectId='cfeb6552-abf6-4fc2-9fe1-d62f76c8579f';
+// Mallee Bird Community of the Murray Darling Depression Bioregion - Protecting mallee habitat for the birds.
+// const projectId='cfeb6552-abf6-4fc2-9fe1-d62f76c8579f';
+// Threatened Mallee Birds: Restoring landscape-scale connectivity to strengthen resilience
+const projectId='c9c9d206-9a9d-41bb-b984-7f692d4474ac';
 let activities = db.activity.find({projectId:projectId, 'externalIds.idType':/MONITOR/});
+var counter = 0;
 while (activities.hasNext()) {
     let activityId = activities.next().activityId;
     db.output.updateMany({activityId:activityId}, {$set:{status:'deleted', note:'Temporarily deleted'}});
+    counter++;
 }
-
+print("Deleted outputs for "+counter+" activities with MONITOR externalIds in project "+projectId);
 // let activities = db.activity.find({projectId:projectId, 'externalIds.idType':/MONITOR/});
 // while (activities.hasNext()) {
 //     let activityId = activities.next().activityId;
