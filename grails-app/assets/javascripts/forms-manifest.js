bf692207ba2ca4bb459f1a42a04b4243c26717f5
sal153 <joseph.salomon@csiro.au>
2025-08-08 09:33:55 +1000

    commit timeentry plugin #3526

--- grails-app/assets/javascripts/forms-manifest.js
+++ grails-app/assets/javascripts/forms-manifest.js
@@ -10,3 +10,5 @@
 //= require geojson2svg/1.2.3/geojson2svg
 //= require feature.js
 //= require activity.js
+//= require jquery.timeentry.package-2.0.1/jquery.plugin.min.js
+//= require jquery.timeentry.package-2.0.1/jquery.timeentry.min.js
