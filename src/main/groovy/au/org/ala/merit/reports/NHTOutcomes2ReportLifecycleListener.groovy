82f7e6354e6665109eef0e0d4a0f2b68acfa15d0
chrisala <chris.godwin.ala@gmail.com>
2026-08-03 14:40:47 +1000

    Added a config item to exclude ST outcomes of outcomes 2 report #3820

--- src/main/groovy/au/org/ala/merit/reports/NHTOutcomes2ReportLifecycleListener.groovy
+++ src/main/groovy/au/org/ala/merit/reports/NHTOutcomes2ReportLifecycleListener.groovy
@@ -1,15 +1,26 @@
 package au.org.ala.merit.reports
 
+import au.org.ala.merit.ProjectConfigurationService
 import au.org.ala.merit.PublicationStatus
+import au.org.ala.merit.config.ProgramConfig
+import org.springframework.beans.factory.annotation.Autowired
 
 class NHTOutcomes2ReportLifecycleListener extends ReportLifecycleListener {
 
     static final String OUTCOMES_1_REPORT_TYPE = "NHT Outcomes 1 Report"
 
+    @Autowired
+    ProjectConfigurationService projectConfigurationService
+
     @Override
     Map getContextData(Map project, Map report, Map activity) {
 
-        boolean hasReportedOnShortTermOutcomes = project.reports?.find{it.activityType == OUTCOMES_1_REPORT_TYPE && it.publicationStatus != PublicationStatus.CANCELLED} != null
+        ProgramConfig config = projectConfigurationService.getProjectConfiguration(project)
+        // If outcomes report 1 has been marked as not required, by default any short term outcomes will
+        // have to be reported on in outcomes report 2.  This behaviour can be overwritten in the program
+        // configuration using the excludeShortTermOutcomesIfReportNotRequired setting.
+        boolean hasReportedOnShortTermOutcomes = project.reports?.find{
+            it.activityType == OUTCOMES_1_REPORT_TYPE && (config.excludeShortTermOutcomesIfReportNotRequired() || it.publicationStatus != PublicationStatus.CANCELLED) } != null
         List<Map> outcomesToReportOn = []
         List shortTermOutcomes = project.custom?.details?.outcomes?.shortTermOutcomes
         if (!hasReportedOnShortTermOutcomes && shortTermOutcomes) {
